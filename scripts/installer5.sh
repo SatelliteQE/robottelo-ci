4d8d598da5e9f36b96558addb7b34393e6e1f063
Lukas Pramuk <lpramuk@redhat.com>
2015-04-22 09:50:02 +0200

    added SATCERT url and SSL conf setup

--- scripts/installer5.sh
+++ scripts/installer5.sh
@@ -12,6 +12,14 @@ fi
 
 # Figure out what version of RHEL the server uses
 OS_VERSION=$(fab -i ~/.ssh/id_hudson_dsa -H root@${SERVER_HOSTNAME} distro_info | grep "rhel [[:digit:]]" | cut -d ' ' -f 2)
+export ADMIN_EMAIL="root@localhost"
+export RHN_PROFILE="robottelo_${SERVER_HOSTNAME}"
+export SSL_PASSWORD=reset
+export SSL_SET_ORG="Red Hat"
+export SSL_SET_ORG_UNIT="Satellite QE"
+export SSL_SET_CITY="Brno"
+export SSL_SET_STATE="BRQ"
+export SSL_SET_COUNTRY="CZ"
 
 if [ ${DISTRIBUTION} = "RELEASED" ]; then
     export ISO_URL="http://download/released/Satellite-${VERSION}-RHEL-${OS_VERSION}/x86_64/ftp-isos/"
