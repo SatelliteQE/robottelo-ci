8f0a89c11cc76645f801435e3bb8f6cbd8bc61c3
Barak Korren <bkorren@redhat.com>
2015-11-24 16:52:56 +0200

    Added Mock configuration by brew target

--- lib/python/koji_mock.py
+++ lib/python/koji_mock.py
@@ -11,25 +11,60 @@ __all__ = ['KojiMock']
 class KojiMock(CustomMockChroot):
     """Wrapper around Koji-managed mock chroots
     """
-    def __init__(self, tag, arch='x86_64', koji_profile='brew'):
+    def __init__(
+        self, tag=None, target=None, arch='x86_64', koji_profile='brew'
+    ):
+        """Create a koji-based mock(1) chroot
+
+        :param str tag: The Koji tag to pull configuration from
+        :param str target: The Koji build target tag to pull configuration from
+        :param str arch: The Koji build architecture
+        :param str koji_profile: The koji configuration profile to use
+
+        One and only one of 'tag' or 'target' must be specified
+        """
         config = self.koji_mock_config(
             tag=tag,
+            target=target,
             arch=arch,
             koji_profile=koji_profile
         )
         super(KojiMock, self).__init__(config=config)
 
     @classmethod
-    def koji_mock_config(cls, tag, arch='x86_64', koji_profile='brew'):
-        """Get the Mock configuration for the given koji tag and arch
+    def koji_mock_config(
+        cls, tag=None, target=None, arch='x86_64', koji_profile='brew'
+    ):
+        """Get the Mock configuration for the given koji tag/target and arch
+
+        :param str tag: The Koji tag to pull configuration from
+        :param str target: The Koji build target tag to pull configuration from
+        :param str arch: The Koji build architecture
+        :param str koji_profile: The koji configuration profile to use
+
+        One and only one of 'tag' or 'target' must be specified
+
+        :rtype: str
+        :returns: The Mock configuration as string
         """
-        koji_cmd = (
+        koji_cmd = [
             cls.koji_exe(),
             '--profile', koji_profile,
             'mock_config',
-            '--tag', tag,
             '--arch', arch,
-        )
+        ]
+        if tag and target:
+            raise RuntimeError(
+                "'tag' and 'target' arguments are mutually exclusive"
+            )
+        elif tag:
+            koji_cmd.extend(('--tag', tag))
+        elif target:
+            koji_cmd.extend(('--target', target))
+        else:
+            raise RuntimeError(
+                "one of 'tag' and 'target' arguments must be specified"
+            )
         output = check_output(koji_cmd)
         return output
 
