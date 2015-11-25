0a429febaefbd5691e1a72f0456b1b989dd390dc
Elyézer Mendes Rezende <elyezermr@gmail.com>
2015-11-25 13:43:43 -0200

    Simplify Satellite 6 automation jobs
    Just create/manage jobs targeting the major version instead of having a
    job for every OS version permutation, like rhel70, rhel71, rhel66,
    rhel67. Now the jobs will be just rhel6 and rhel7 and they will always
    target the last minor version.
    
    Also drop SAM jobs and provide a better name for the yaml file.

--- scripts/automation.sh
+++ scripts/automation.sh
@@ -1,15 +1,8 @@
 pip install -U -r requirements.txt nose PyVirtualDisplay
 
-# Project on YAML file is satellite6 or sam but robottelo just accept sam or
-# sat as a project. Adjust the product variable properly
-if [ "${PRODUCT}" = 'satellite6' ]; then
-    PRODUCT='sat'
-fi
-
 cp ${ROBOTTELO_CONFIG} ./robottelo.properties
 
 sed -i "s/{server_hostname}/${SERVER_HOSTNAME}/" robottelo.properties
-sed -i "s/^project.*/project=${PRODUCT}/" robottelo.properties
 
 # Robottelo logging configuration
 sed -i "s/'\(robottelo\).log'/'\1-${ENDPOINT}.log'/" logging.conf
