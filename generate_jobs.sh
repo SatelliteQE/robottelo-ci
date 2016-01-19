3e35e719f22dd9c273d66fc736126625bd4ebecd
Eric D Helms <ericdhelms@gmail.com>
2016-01-19 11:51:08 -0500

    Add travis config to test building all jobs for generation failures.

--- generate_jobs.sh
+++ generate_jobs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-jenkins-jobs --conf jenkins_jobs.ini  -l debug test -r -o /tmp/jobs .:foreman-infra
+jenkins-jobs --conf jenkins_jobs.ini  -l debug test -r -o /tmp/jobs jobs:foreman-infra
