164647f61ffc69c2df162eed83f1e2a22865a63a
Eric D Helms <ericdhelms@gmail.com>
2016-02-04 09:45:25 -0500

    Add release pipeline jobs and flow for el 5, 6 & 7

--- scripts/promote_content_view.sh
+++ scripts/promote_content_view.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -xe
 
+version_id=`hammer --username ${SATELLITE_USERNAME} --password ${SATELLITE_PASSWORD} --server ${SATELLITE_SERVER} content-view version list --organization "${organization}" --content-view "${content_view_name}" | awk -F'|' '{print $1}' | sort -n  | tac | head -n 1`
+
 hammer --username ${SATELLITE_USERNAME} --password ${SATELLITE_PASSWORD} --server ${SATELLITE_SERVER} \
     content-view version promote --to-lifecycle-environment "${lifecycle_environment}" --organization \
     "${organization}" --content-view "${content_view_name}" --id "${version_id}"
--- scripts/publish_content_view.sh
+++ scripts/publish_content_view.sh
@@ -1,8 +1,3 @@
 #!/bin/bash -xe
 
 hammer --username ${SATELLITE_USERNAME} --password ${SATELLITE_PASSWORD} --server ${SATELLITE_SERVER} content-view publish --organization "${organization}" --name "${content_view_name}"
-version_id=`hammer --username ${SATELLITE_USERNAME} --password ${SATELLITE_PASSWORD} --server ${SATELLITE_SERVER} content-view version list --organization "${organization}" --content-view "${content_view_name}" | awk -F'|' '{print $1}' | sort -n  | tac | head -n 1`
-
-cat > version_properties <<EOL
-version_id=${version_id}
-EOL
