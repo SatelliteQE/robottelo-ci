b32e9a2ea101042c5ae9b9d3d7bcd59394c4ee07
Elyézer Mendes Rezende <elyezermr@gmail.com>
2016-02-17 16:18:18 -0200

    Create a test run from a template
    Use Satellite 6 Automation test template to create a test run. The test
    template will make sure to add the non automated test cases.

--- scripts/satellite6-betelgeuse-test-run.sh
+++ scripts/satellite6-betelgeuse-test-run.sh
@@ -1,17 +1,22 @@
+TEST_TEMPLATE_ID="Satellite 6 Automation"
+
 betelgeuse -j auto test-run \
     --path tier1-results.xml \
     --test-run-id "${TEST_RUN_ID} - Tier 1" \
+    --test-template-id "${TEST_TEMPLATE_ID}" \
     --user ${POLARION_USER} \
     ${POLARION_DEFAULT_PROJECT}
 
 betelgeuse -j auto test-run \
     --path tier2-results.xml \
     --test-run-id "${TEST_RUN_ID} - Tier 2" \
+    --test-template-id "${TEST_TEMPLATE_ID}" \
     --user ${POLARION_USER} \
     ${POLARION_DEFAULT_PROJECT}
 
 betelgeuse -j auto test-run \
     --path tier3-results.xml \
     --test-run-id "${TEST_RUN_ID} - Tier 3" \
+    --test-template-id "${TEST_TEMPLATE_ID}" \
     --user ${POLARION_USER} \
     ${POLARION_DEFAULT_PROJECT}
