93be3228d86423590ceb171088177d0985a1b007
Eric D Helms <ericdhelms@gmail.com>
2016-02-29 21:11:39 -0500

    Allow plugins and non-plugins to use merge request

--- scripts/merge_request.sh
+++ scripts/merge_request.sh
@@ -1,8 +1,15 @@
 #!/bin/bash -ex
 
 if [ -n "${gitlabSourceBranch}" ]; then
-  cd plugin
+  if [ -d plugin ];then
+    pushd plugin
+  fi
+
   git remote add pr https://$GIT_HOSTNAME/${gitlabSourceRepoName}.git
   git fetch pr
   git merge pr/${gitlabSourceBranch}
+
+  if [ -d '../plugin' ];then
+    popd
+  fi
 fi
