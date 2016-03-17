ddb895ba7532c921287939f37ab570d5667c2070
jyejare <jyejare@redhat.com>
2016-03-17 18:29:21 +0530

    Upgrade Specific Satellite and Capsule/ Personal Upgrade

--- scripts/satellite6_upgrader.sh
+++ scripts/satellite6_upgrader.sh
@@ -8,14 +8,30 @@ elif [ "${OS}" = 'rhel6' ]; then
 fi
 
 # Source the Variables from files
-source "${OPENSTACK_CONFIG}"
+source "${RHEV_CONFIG}"
 source "${SATELLITE6_REPOS_URLS}"
 source "${SUBSCRIPTION_CONFIG}"
 
-# Export required Environment variables
-export BASE_URL="${SATELLITE6_REPO}"
-export CAPSULE_URL="${CAPSULE_REPO}"
-export TOOLS_URL="${TOOLS_REPO}"
-
-# Run upgrade
-fab -u root product_upgrade:"${UPGRADE_PRODUCT}","${SSH_KEY_NAME}","${SATELLITE_INSTANCE}","${SATELLITE_IMAGE}","${IMAGE_FLAVOR}","${CAPSULE_INSTANCE}","${CAPSULE_IMAGE}","${IMAGE_FLAVOR}"
+if [ -n "${SATELLITE_HOSTNAME}" ]; then
+	if [ "${DISTRIBUTION}" = 'CDN' ]; then
+		# Run upgrade without compose urls
+		fab -u root product_upgrade:"${UPGRADE_PRODUCT}"
+	elif [ "${DISTRIBUTION}" = 'DOWNSTREAM' ]; then
+		# Export required Environment variables
+		export BASE_URL="${SATELLITE6_REPO}"
+		export CAPSULE_URL="${CAPSULE_REPO}"
+		# Run upgrade with above compose urls
+		fab -u root product_upgrade:"${UPGRADE_PRODUCT}"
+	fi
+elif [ -n "${SATELLITE_IMAGE}" ]; then
+	if [ "${DISTRIBUTION}" = 'CDN' ]; then
+		# Run upgrade without compose urls
+		fab -u root product_upgrade:"${UPGRADE_PRODUCT}","${SATELLITE_IMAGE}","${CAPSULE_IMAGE}"
+	elif [ "${DISTRIBUTION}" = 'DOWNSTREAM' ]; then
+		# Export required Environment variables
+		export BASE_URL="${SATELLITE6_REPO}"
+		export CAPSULE_URL="${CAPSULE_REPO}"
+		# Run upgrade with above compose urls
+		fab -u root product_upgrade:"${UPGRADE_PRODUCT}","${SATELLITE_IMAGE}","${CAPSULE_IMAGE}"
+	fi
+fi
