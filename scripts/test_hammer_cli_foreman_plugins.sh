1e6ac3f32099692d1c0415e8928faab7f6dc3f7b
Chris Peters <chrispeters.psu@gmail.com>
2016-03-29 13:10:46 -0400

    Add unit test job for satellite6 hammer_cli_foreman_discovery

--- scripts/test_hammer_cli_foreman_plugins.sh
+++ scripts/test_hammer_cli_foreman_plugins.sh
@@ -9,15 +9,9 @@ rvm gemset empty --force
 gem update --no-ri --no-rdoc
 gem install bundler --no-ri --no-rdoc
 
-echo "gem 'rake'" >> Gemfile.local
-echo "gem 'rubocop'" >> Gemfile.local
-echo "gem 'mocha'" >> Gemfile.local
-echo "gem 'minitest', '4.7.4'" >> Gemfile.local
-echo "gem 'minitest-spec-context'" >> Gemfile.local
-
 echo "gem 'hammer_cli', :git => 'https://${GIT_HOSTNAME}/${GIT_ORGANIZATION}/hammer_cli.git', :ref => '${gitlabTargetBranch}'" > Gemfile.local
 echo "gem 'hammer_cli_foreman', :git => 'https://${GIT_HOSTNAME}/${GIT_ORGANIZATION}/hammer_cli_foreman.git', :ref => '${gitlabTargetBranch}'" >> Gemfile.local
 
 bundle install
 
-bundle exec rake test TESTOPTS="-v"
+bundle exec rake ci:setup:minitest test TESTOPTS="-v"
