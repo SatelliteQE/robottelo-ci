a6d4803a8319e59268c809592a90b65d3ce05384
Elyézer Rezende <elyezermr@gmail.com>
2016-04-01 17:39:30 -0300

    Generate test-run for tier4
    Include tier 4 for generating a Polarion test run. Also remove the template and
    use Empty instead.

--- scripts/satellite6-betelgeuse-test-run.sh
+++ scripts/satellite6-betelgeuse-test-run.sh
@@ -1,4 +1,4 @@
-TEST_TEMPLATE_ID="Satellite 6 Automation"
+TEST_TEMPLATE_ID="Empty"
 
 betelgeuse -j auto test-run \
     --path tier1-results.xml \
@@ -17,6 +17,13 @@ betelgeuse -j auto test-run \
 betelgeuse -j auto test-run \
     --path tier3-results.xml \
     --test-run-id "${TEST_RUN_ID} - Tier 3" \
+    --test-template-id "${TEST_TEMPLATE_ID}" \
+    --user ${POLARION_USER} \
+    ${POLARION_DEFAULT_PROJECT}
+
+betelgeuse -j auto test-run \
+    --path tier4-results.xml \
+    --test-run-id "${TEST_RUN_ID} - Tier 4" \
     --test-template-id "${TEST_TEMPLATE_ID}" \
     --user ${POLARION_USER} \
     ${POLARION_DEFAULT_PROJECT}
