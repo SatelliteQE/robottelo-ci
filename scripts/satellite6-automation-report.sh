d6581a28c97f5889e1c3f993947ad68db5c5be1d
Elyézer Rezende <elyezermr@gmail.com>
2016-05-05 16:56:45 -0300

    Update satellite6-automation-report-downstream
    The javascript and CSS was not being rendered by email clients. Revert the
    email to text and attach the html report.

--- scripts/satellite6-automation-report.sh
+++ scripts/satellite6-automation-report.sh
@@ -1,3 +1,4 @@
 pip install click jinja2
 
-python lib/python/satellite6-automation-report.py *.xml -o html > report
+python lib/python/satellite6-automation-report.py *.xml > report.txt
+python lib/python/satellite6-automation-report.py -o html *.xml > report.html
