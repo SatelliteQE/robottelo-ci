SatelliteQE/robottelo-ci

commit b2d7ead44c29f28f2a557a7c101b217d57152d02

Introducing satellite reset after the end of tier jobs (#279)
* This approach is voted by the team to get the satellite to clean slate as
having a large amount of unnecessary data causes slowness

scripts/satellite6-automation.sh
@@ -40,6 +40,13 @@ fi
 
 if [ "${ENDPOINT}" != "rhai" ]; then
     set +e
+    # Reset satellite at the start of tier2, tier3, tier4 jobs
+    if [[ "${ENDPOINT}" =~ tier[234] ]]; then 
+        echo "Resetting Satellite..."
+        ssh root@"${SERVER_HOSTNAME}" "satellite-installer --reset"
+        echo "Satellite Reset Complete"
+    if
+
     # Run parallel tests
     $(which py.test) -v --junit-xml="${ENDPOINT}-parallel-results.xml" -n "${ROBOTTELO_WORKERS}" \
         --boxed -m "${ENDPOINT} and not run_in_one_thread and not stubbed" \

commit bacea39725c60bc7069e00ff5f54e52b3d38b96a

Fix typo in reset command introduced in #279

scripts/satellite6-automation.sh
@@ -45,7 +45,7 @@ if [ "${ENDPOINT}" != "rhai" ]; then
         echo "Resetting Satellite..."
         ssh root@"${SERVER_HOSTNAME}" "satellite-installer --reset"
         echo "Satellite Reset Complete"
-    if
+    fi
 
     # Run parallel tests
     $(which py.test) -v --junit-xml="${ENDPOINT}-parallel-results.xml" -n "${ROBOTTELO_WORKERS}" \