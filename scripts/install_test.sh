476f09a8f8810f6c1c89785f1f5943cc562b3c1b
Eric D Helms <ericdhelms@gmail.com>
2016-06-07 18:46:02 -0400

    Keep installation boxes up after running test

--- scripts/install_test.sh
+++ scripts/install_test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -xe
 
 ssh jenkins@$LIBVIRT_HOST "cd sat-deploy && git -c http.sslVerify=false fetch origin && git reset origin/master --hard"
+ssh jenkins@$LIBVIRT_HOST "cd sat-deploy/lago/environment-rhel${rhel} && lago revert initial"
 ssh jenkins@$LIBVIRT_HOST "cd sat-deploy/lago/environment-rhel${rhel} && lago snapshot initial"
 ssh jenkins@$LIBVIRT_HOST "cd sat-deploy/lago && ./install-satellite.rb rhel${rhel}"
