954dad0ed6c80bae87a856a03a88a1c5b136060f
Eric D Helms <ericdhelms@gmail.com>
2016-06-10 12:32:39 -0400

    Add kafo

---
+++ scripts/test_kafo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -ex
+
+APP_ROOT=`pwd`
+
+# RVM Ruby environment
+. /etc/profile.d/rvm.sh
+# Use a gemset unique to each executor to enable parallel builds
+gemset=$(echo ${JOB_NAME} | cut -d/ -f1)-${EXECUTOR_NUMBER}
+rvm use ruby-${ruby}@${gemset} --create
+rvm gemset empty --force
+# Update any gems from the global gemset
+#gem update --no-ri --no-rdoc
+gem install bundler --no-ri --no-rdoc
+
+# Puppet environment
+cat >> $APP_ROOT/Gemfile << EOF
+gem 'facter'
+gem 'puppet', '~> 3.8.0'
+EOF
+
+bundle install
+bundle exec rake jenkins:unit
