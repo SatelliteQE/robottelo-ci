d3ad783843b73d75d3f5141a05946ec64cc9a442
Eric D Helms <eric.d.helms@gmail.com>
2016-06-16 13:31:04 -0400

    Remove initial snapshot from install test (#303)
    The install test has been updated to handle snapshotting within
    the library itself versus through Jenkins.

--- scripts/install_test.sh
+++ scripts/install_test.sh
@@ -1,6 +1,4 @@
 #!/bin/bash -xe
 
 ssh jenkins@$LIBVIRT_HOST "cd sat-deploy && git -c http.sslVerify=false fetch origin && git reset origin/master --hard"
-ssh jenkins@$LIBVIRT_HOST "cd sat-deploy/lago/environment-rhel${rhel} && lago revert initial"
-ssh jenkins@$LIBVIRT_HOST "cd sat-deploy/lago/environment-rhel${rhel} && lago snapshot initial"
 ssh jenkins@$LIBVIRT_HOST "cd sat-deploy/lago && ./install-satellite.rb rhel${rhel}"
