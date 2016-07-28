f4f3433445de4c8654a27eae2a591749c435d976
sthirugn <sthirugn@redhat.com>
2016-07-28 16:22:37 -0400

    Create iteration for betelgeuse jobs (#326)
    1. Automatically create iteration for each run.
    2. Link the test runs to the newly created iteration.
    3. After the test runs are linked, mark the iteration done.

--- scripts/satellite6-betelgeuse-test-run.sh
+++ scripts/satellite6-betelgeuse-test-run.sh
@@ -1,5 +1,14 @@
 TEST_TEMPLATE_ID="Empty"
 
+# Create a new iteration for the current run
+if [ ! -z "$RELEASE" ]; then
+    betelgeuse test-plan --name "${TEST_RUN_ID}" --parent-name "${RELEASE}" \
+        --plan-type iteration "${POLARION_DEFAULT_PROJECT}"
+else
+    betelgeuse test-plan --name "${TEST_RUN_ID}" \
+        --plan-type iteration "${POLARION_DEFAULT_PROJECT}"
+fi
+
 for path in tier{1,2,3,4}-{parallel,sequential}-results.xml; do
     case "$path" in
         tier1*)
@@ -22,5 +31,15 @@ for path in tier{1,2,3,4}-{parallel,sequential}-results.xml; do
         --test-template-id "${TEST_TEMPLATE_ID}" \
         --user "${POLARION_USER}" \
         --source-code-path tests/foreman \
+        --custom-fields isautomated=True \
+        --custom-fields arch=x8664 \
+        --custom-fields variant=server \
+        --custom-fields plannedin="${TEST_RUN_ID}" \
         "${POLARION_DEFAULT_PROJECT}"
 done
+
+# Mark the iteration done
+betelgeuse test-plan \
+    --name "${TEST_RUN_ID}" \
+    --custom-fields status=done \
+    "${POLARION_DEFAULT_PROJECT}"
