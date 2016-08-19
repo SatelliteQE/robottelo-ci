dee42b8421e4e98990cfa3675bf11f811b464394
Elyézer Rezende <elyezermr@gmail.com>
2016-08-19 16:49:01 -0300

    Fix foreman-debug capturing post-build step (#347)

--- scripts/satellite6-automation-foreman-debug.sh
+++ scripts/satellite6-automation-foreman-debug.sh
@@ -1,10 +1,14 @@
 # Grab log files
 rm -rf foreman-debug.tar.xz
 
+if [ ! "${SERVER_HOSTNAME}" ]; then
+    SERVER_HOSTNAME="$(grep SERVER_HOSTNAME build_env.properties | cut -d= -f2-)"
+fi
+
 # Disable error checking, for more information check the related issue
 # http://projects.theforeman.org/issues/13442
 set +e
-ssh root@${PROVISIONING_HOST} foreman-debug -g -q -d ~/foreman-debug
+ssh "root@${SERVER_HOSTNAME}" foreman-debug -g -q -d ~/foreman-debug
 set -e
 
-scp -r root@${PROVISIONING_HOST}:~/foreman-debug/foreman-debug.tar.xz .
+scp -r "root@${SERVER_HOSTNAME}:~/foreman-debug/foreman-debug.tar.xz" .
--- scripts/satellite6-provisioning.sh
+++ scripts/satellite6-provisioning.sh
@@ -30,8 +30,6 @@ if [ "${DISTRIBUTION}" == 'satellite6-zstream' -o "${DISTRIBUTION}" == 'satellit
     export TARGET_IMAGE="${BTARGET_IMAGE}"
 fi
 
-fab -H "root@${PROVISIONING_HOST}" "product_install:${DISTRIBUTION},create_vm=true,sat_cdn_version=${SATELLITE_VERSION},test_in_stage=${STAGE_TEST}"
-
 # Write a properties file to allow passing variables to other build steps
 echo "SERVER_HOSTNAME=${SERVER_HOSTNAME}" > build_env.properties
 echo "TOOLS_REPO=${TOOLS_URL}" >> build_env.properties
@@ -40,6 +38,12 @@ echo "NETMASK=${NETMASK}" >> build_env.properties
 echo "GATEWAY=${GATEWAY}" >> build_env.properties
 echo "BRIDGE=${BRIDGE}" >> build_env.properties
 echo "DISCOVERY_ISO=${DISCOVERY_ISO}" >> build_env.properties
+
+# Run installation after writing the build_env.properties to make sure the
+# values are available for the post build actions, specially the foreman-debug
+# capturing.
+fab -H "root@${PROVISIONING_HOST}" "product_install:${DISTRIBUTION},create_vm=true,sat_cdn_version=${SATELLITE_VERSION},test_in_stage=${STAGE_TEST}"
+
 echo
 echo "========================================"
 echo "Server information"
