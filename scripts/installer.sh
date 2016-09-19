b1d0ab79dfca46c4193bb241fee4c1f62327b0aa
kbidarkar <kbidarka@redhat.com>
2016-09-19 19:56:09 +0530

    Add Satellite6.3 support to satellite-installer job
    a) Now satellite-installer job also uses INTERNAL, INTERNAL AK and
       INTERNAL REPOFILE
    
    b) For Satellite6-automation, DOGFOOD_ACTIVATIONKEY and
       REPO_FILE_URL are now automatically calculated in the
       Subscription config-file depending upon the DISTRO variable, for
       installations via jenkins.
    
    c) For Satellite-installer, as the DISTRO value is not populated,
       DOGFOOD_ACTIVATIONKEY and REPO_FILE_URL  is calculated depending
       upon OS_VERSION available in installer.sh script used by
       satellite-installer job.
    
    d) Now DOGFOOD ENV_VAR and REPO_FILE_URL are specified in the config-file,
       Subscription Config File.

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -3,12 +3,19 @@ pip install -U -r requirements.txt
 # Figure out what version of RHEL the server uses
 export OS_VERSION=$(fab -H root@${SERVER_HOSTNAME} distro_info | grep "rhel [[:digit:]]" | cut -d ' ' -f 2)
 
-
 source ${INSTALL_CONFIG}
 source ${PROXY_CONFIG}
 # OS_VERSION needs to be defined before sourcing SATELLITE6_REPOS_URLS
 source ${SATELLITE6_REPOS_URLS}
 source ${SUBSCRIPTION_CONFIG}
+
+# Populate DOGFOOD_AACTIVATIONKEY ENV_VAR depending upon the OS_VERSION
+if [ "${OS_VERSION}" = '7' ]; then
+    export DOGFOOD_ACTIVATIONKEY="${DOGFOOD_RHEL7_AK}"
+elif [ "${OS_VERSION}" = '6' ]; then
+    export DOGFOOD_ACTIVATIONKEY="${DOGFOOD_RHEL6_AK}"
+fi
+
 if [ "$STAGE_TEST" = 'false' ]; then
     source ${SUBSCRIPTION_CONFIG}
 else
@@ -23,9 +30,23 @@ if [ ${PARTITION_DISK} = "true" ]; then
     fab -H root@${SERVER_HOSTNAME} partition_disk
 fi
 
+# Assign DISTRIBUTION to trigger things appropriately from automation-tools.
+if [ "${SATELLITE_DISTRIBUTION}" = 'INTERNAL' ]; then
+    export DISTRIBUTION="satellite6-downstream"
+elif [ "${SATELLITE_DISTRIBUTION}" = 'GA' ]; then
+    export DISTRIBUTION="satellite6-cdn"
+elif [ "${SATELLITE_DISTRIBUTION}" = 'BETA' ]; then
+    export DISTRIBUTION="satellite6-beta"
+elif [ "${SATELLITE_DISTRIBUTION}" = 'UPSTREAM' ]; then
+    export DISTRIBUTION="satellite6-upstream"
+elif [ "${SATELLITE_DISTRIBUTION}" = 'INTERNAL REPOFILE' ]; then
+    export DISTRIBUTION="satellite6-repofile"
+elif [ "${SATELLITE_DISTRIBUTION}" = 'INTERNAL AK' ]; then
+    export DISTRIBUTION="satellite6-activationkey"
+fi
 
 # ISOs require a specific URL
-if [ ${DISTRIBUTION} = "ISO" ]; then
+if [ ${SATELLITE_DISTRIBUTION} = "ISO" ]; then
     # If user provided custom baseurl, use it otherwise use the default
     if [ ! -z "$SATELLITE6_CUSTOM_BASEURL" ]; then
         export ISO_URL="${SATELLITE6_CUSTOM_BASEURL}"
@@ -35,7 +56,7 @@ if [ ${DISTRIBUTION} = "ISO" ]; then
 fi
 
 # This is only used for downstream builds
-if [ ${DISTRIBUTION} = "DOWNSTREAM" ]; then
+if [ ${SATELLITE_DISTRIBUTION} = "INTERNAL" ]; then
     # If user provided custom baseurl, use it otherwise use the default
     if [ ! -z "$SATELLITE6_CUSTOM_BASEURL" ]; then
         export BASE_URL="${SATELLITE6_CUSTOM_BASEURL}"
