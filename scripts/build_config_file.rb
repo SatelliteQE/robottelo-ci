48250d46ab10fc37fa2e7c8de06c2a0b2eeb9f1f
Eric D Helms <ericdhelms@gmail.com>
2016-09-22 14:07:53 -0400

    Compare all repositories in a content view

--- scripts/build_config_file.rb
+++ scripts/build_config_file.rb
@@ -2,18 +2,12 @@
 
 require 'yaml'
 
-content_view = ENV['content_view_name']
-type = content_view.split(' ')[0]
-rhel = content_view.split(' ')[1]
-
 config = {
   'organization' => ENV['organization'],
   'username' => ENV['SATELLITE_USERNAME'],
   'password' => ENV['SATELLITE_PASSWORD'],
   'server' => ENV['SATELLITE_SERVER'],
-  'product' => "Red Hat #{type} 6.2 Composes",
-  'repository' => "#{rhel} #{type} x86_64 os",
-  'content_view' => content_view,
+  'content_view' => ENV['content_view_name'],
   'lifecycle_environment' => ENV['lifecycle_environment'],
   'from_lifecycle_environment' => ENV['from_lifecycle_environment']
 }
--- scripts/compare_content_views.rb
+++ scripts/compare_content_views.rb
@@ -5,70 +5,100 @@ require 'json'
 require 'openssl'
 require 'net/http'
 
-CONFIG = YAML.load_file('config.yaml')
 
-def base_command
-  [
-    'hammer --output yaml',
-    "--username #{CONFIG['username']}",
-    "--password #{CONFIG['password']}",
-    "--server #{CONFIG['server']}"
-  ]
+class ContentViewCompare
+
+  attr_reader :config
+
+  def initialize(config)
+    @config = config
+  end
+
+  def base_command
+    [
+      'hammer --output yaml',
+      "--username #{@config['username']}",
+      "--password #{@config['password']}",
+      "--server #{@config['server']}"
+    ]
+  end
+
+  def find_version(environment)
+    cmd = [
+      "content-view version list",
+      "--content-view '#{@config['content_view']}'",
+      "--organization '#{@config['organization']}'",
+      "--environment '#{environment}'"
+    ]
+    cmd = base_command.concat(cmd)
+
+    versions = YAML.load(`#{cmd.join(' ')}`)
+    versions.first['ID'] unless versions.empty?
+  end
+
+  def find_repositories(version_id)
+    cmd = [
+      'content-view version info',
+      "--id #{version_id}"
+    ]
+    cmd = base_command.concat(cmd)
+
+    repositories = YAML.load(`#{cmd.join(' ')}`)
+    repositories['Repositories']
+  end
+
+  def find_packages(version_id, repo_id)
+    return [] unless version_id
+    cmd = [
+      "package list",
+      "--repository-id '#{repo_id}'"
+    ]
+    cmd = base_command.concat(cmd)
+
+    YAML.load(`#{cmd.join(' ')}`)
+  end
+
+  def compare
+    from_version = find_version(@config['from_lifecycle_environment'])
+    to_version = find_version(@config['lifecycle_environment'])
+
+    from_repositories = find_repositories(from_version)
+    to_repositories = find_repositories(to_version)
+
+    reports = from_repositories.collect do |index, repo|
+      from_packages = find_packages(from_version, repo['ID'])
+      to_packages = find_packages(to_version, to_repositories[index]['ID']).collect { |package| package['Filename'] }
+
+      new_packages = from_packages.select do |package|
+        !to_packages.include?(package['Filename'])
+      end
+
+      report = []
+
+      new_packages.each do |package|
+        uri = URI("#{@config['server']}/katello/api/v2/packages/#{package['ID']}")
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+        request = Net::HTTP::Get.new(uri.request_uri)
+        request.basic_auth @config['username'], @config['password']
+
+        response = JSON.parse(http.request(request).body)
+
+        report << {"package" => package['Filename'], "sourcerpm" => response['sourcerpm']}
+      end
+
+      report = {"#{repo['Name']}" => {'packages' => report}}
+    end
+
+    File.open("package_report_#{@config['content_view'].gsub(' ', '_')}.yaml", 'w') do |file|
+      file.write(reports.to_yaml(:Indent => 4))
+    end
+  end
 end
 
-def find_version(environment)
-  cmd = [
-    "content-view version list",
-    "--content-view '#{CONFIG['content_view']}'",
-    "--organization '#{CONFIG['organization']}'",
-    "--environment '#{environment}'"
-  ]
-  cmd = base_command.concat(cmd)
+config = YAML.load_file('config.yaml')
 
-  versions = YAML.load(`#{cmd.join(' ')}`)
-  versions.first['ID'] unless versions.empty?
-end
-
-def find_packages(version_id)
-  return [] unless version_id
-  cmd = [
-    "package list",
-    "--organization #{CONFIG['organization']}",
-    "--repository '#{CONFIG['repository']}'",
-    "--product '#{CONFIG['product']}'",
-    "--content-view-version-id #{version_id}"
-  ]
-  cmd = base_command.concat(cmd)
-
-  YAML.load(`#{cmd.join(' ')}`)
-end
-
-from_version = find_version(CONFIG['from_lifecycle_environment'])
-to_version = find_version(CONFIG['lifecycle_environment'])
-
-from_packages = find_packages(from_version)
-to_packages = find_packages(to_version).collect { |package| package['Filename'] }
-
-new_packages = from_packages.select do |package|
-  !to_packages.include?(package['Filename'])
-end
-
-report = []
-
-new_packages.each do |package|
-  uri = URI("#{CONFIG['server']}/katello/api/v2/packages/#{package['ID']}")
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-  request = Net::HTTP::Get.new(uri.request_uri)
-  request.basic_auth CONFIG['username'], CONFIG['password']
-
-  response = JSON.parse(http.request(request).body)
-
-  report << {"package" => package['Filename'], "sourcerpm" => response['sourcerpm']}
-end
-
-File.open("package_report_#{CONFIG['content_view'].gsub(' ', '_')}.yaml", 'w') do |file|
-  file.write(report.to_yaml(:Indent => 4))
-end
+cv_compare = ContentViewCompare.new(config)
+cv_compare.compare
