a06019e97b6a30ee91808698650e41a0a3c551f3
Eric D Helms <ericdhelms@gmail.com>
2016-09-28 18:02:19 -0400

    Add playbooks and workflow job to release 6.3, 6.2 and nightly from compose

---
+++ workflows/6.2/releasePipelineAttributes.groovy
@@ -0,0 +1,22 @@
+def compose_versions = ['7', '6', '5']
+
+def os_versions = ['7', '6']
+
+def satellite_short_version = 62
+def satellite_version = '6.2.0'
+
+def products = [
+    'Red Hat Satellite 6.2 Composes',
+    'Red Hat Capsule 6.2 Composes',
+    'Red Hat Satellite Tools 6.2 Composes'
+]
+
+def content_views = [
+    'Satellite 6.2 RHEL7',
+    'Satellite 6.2 RHEL6',
+    'Capsule 6.2 RHEL7',
+    'Capsule 6.2 RHEL6',
+    'Tools 6.2 RHEL7',
+    'Tools 6.2 RHEL6',
+    'Tools 6.2 RHEL5'
+]
