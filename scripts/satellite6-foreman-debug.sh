8c586ccb8fbefa2d5bca18739bb49643e3394fce
kbidarkar <kbidarka@redhat.com>
2016-11-21 14:26:59 +0530

    Disable strict hostkey checks for foreman_debug task

--- scripts/satellite6-foreman-debug.sh
+++ scripts/satellite6-foreman-debug.sh
@@ -1,14 +1,11 @@
 # Grab log files
 rm -rf foreman-debug.tar.xz
-
 if [ ! "${SERVER_HOSTNAME}" ]; then
     SERVER_HOSTNAME="$(grep SERVER_HOSTNAME build_env.properties | cut -d= -f2-)"
 fi
-
 # Disable error checking, for more information check the related issue
 # http://projects.theforeman.org/issues/13442
 set +e
-ssh "root@${SERVER_HOSTNAME}" foreman-debug -g -q -d "~/foreman-debug"
+ssh -o StrictHostKeyChecking=no "root@${SERVER_HOSTNAME}" foreman-debug -g -q -d "~/foreman-debug"
 set -e
-
-scp -r "root@${SERVER_HOSTNAME}:~/foreman-debug.tar.xz" .
+scp -o StrictHostKeyChecking=no -r "root@${SERVER_HOSTNAME}:~/foreman-debug.tar.xz" .
