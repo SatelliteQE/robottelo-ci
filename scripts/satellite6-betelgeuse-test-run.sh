57f7ec3219fcbe801af02779f0b02d2611345844
Og B. Maciel <omaciel@ogmaciel.com>
2016-12-20 15:56:59 -0500

    Fixing requested changes from review.

--- scripts/satellite6-betelgeuse-test-run.sh
+++ scripts/satellite6-betelgeuse-test-run.sh
@@ -1,31 +1,3 @@
-rm -rf pylarion
-git clone ${PYLARION_REPO_URL}
-
-# Install pylarion and its dependencies
-cd pylarion
-git checkout origin/satelliteqe-pylarion
-pip install -r requirements.txt
-pip install .
-cd ..
-
-rm -rf betelgeuse
-git clone https://github.com/SatelliteQE/betelgeuse.git
-cd betelgeuse
-git checkout xml-test-case
-pip install -r requirements.txt
-pip install .
-cd ..
-
-cat > .pylarion <<EOF
-[webservice]
-url=${POLARION_URL}
-user=${POLARION_USER}
-password=${POLARION_PASSWORD}
-default_project=${POLARION_DEFAULT_PROJECT}
-svn_repo=${POLARION_SVN_REPO}
-use_logstash=false
-EOF
-
 TEST_TEMPLATE_ID="Empty"
 
 # Create a new iteration for the current run
@@ -43,29 +15,29 @@ SANITIZED_ITERATION_ID="$(echo ${TEST_RUN_ID} | sed 's|\.|_|g' | sed 's| |_|g')"
 # Prepare the XML files
 for tier in $(seq 1 4)
 do
-  for run in parallel sequential
-  do
-    betelgeuse --token-prefix="@" xml-test-run \
-        --custom-fields="isautomated=true" \
-        --custom-fields="arch=x8664" \
-        --custom-fields="variant=server" \
-        --custom-fields="plannedin=${SANITIZED_ITERATION_ID}" \
-        --response-property="${POLARION_SELECTOR}" \
-        --test-run-id "${TEST_RUN_ID} - ${run} - Tier ${tier}" \
-        ./tier${tier}-${run}-results.xml \
-        tests/foreman \
-        "${POLARION_USER}" \
-        "${POLARION_DEFAULT_PROJECT}" \
-        polarion-tier${tier}-${run}-results.xml
-    curl -k -u ${POLARION_USER}:${POLARION_PASSWORD} \
-    -X POST \
-    -F file=@polarion-tier${tier}-${run}-results.xml \
-    "${POLARION_URL}import/xunit"
-  done
+   for run in parallel sequential
+   do
+       betelgeuse --token-prefix="@" xml-test-run \
+           --custom-fields="isautomated=true" \
+           --custom-fields="arch=x8664" \
+           --custom-fields="variant=server" \
+           --custom-fields="plannedin=${SANITIZED_ITERATION_ID}" \
+           --response-property="${POLARION_SELECTOR}" \
+           --test-run-id "${TEST_RUN_ID} - ${run} - Tier ${tier}" \
+           ./tier${tier}-${run}-results.xml \
+           tests/foreman \
+           "${POLARION_USER}" \
+           "${POLARION_DEFAULT_PROJECT}" \
+           polarion-tier${tier}-${run}-results.xml
+       curl -k -u ${POLARION_USER}:${POLARION_PASSWORD} \
+           -X POST \
+           -F file=@polarion-tier${tier}-${run}-results.xml \
+           "${POLARION_URL}import/xunit"
+   done
 done
 
 # Mark the iteration done
 betelgeuse test-plan \
-    --name "${TEST_RUN_ID}" \
-    --custom-fields status=done \
-    "${POLARION_DEFAULT_PROJECT}"
+     --name "${TEST_RUN_ID}" \
+     --custom-fields status=done \
+     "${POLARION_DEFAULT_PROJECT}"
