e01563e80ac451317eabe5c1804b3518ece865f4
Eric D Helms <ericdhelms@gmail.com>
2017-02-03 11:12:38 -0500

    Use RVM 2.2 ruby for unit tests

--- scripts/test_tool_belt.sh
+++ scripts/test_tool_belt.sh
@@ -4,7 +4,7 @@
 . /etc/profile.d/rvm.sh
 # Use a gemset unique to each executor to enable parallel builds
 gemset=$(echo ${JOB_NAME} | cut -d/ -f1)-${EXECUTOR_NUMBER}
-rvm use ruby-2.2.0@${gemset} --create
+rvm use ruby-2.2@${gemset} --create
 rvm gemset empty --force
 gem update --no-ri --no-rdoc
 gem install bundler --no-ri --no-rdoc
