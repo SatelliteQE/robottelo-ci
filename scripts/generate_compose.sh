dc7675853a117b61dc18e65a5e910235a24704fa
Eric D Helms <ericdhelms@gmail.com>
2017-02-13 15:47:53 -0500

    Compose generation assumes compose directory

--- scripts/generate_compose.sh
+++ scripts/generate_compose.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 ssh jenkins@$COMPOSE_HOST "cd satellite-packaging && git -c http.sslVerify=false fetch origin && git checkout origin/SATELLITE-${version}"
-ssh jenkins@$COMPOSE_HOST "cd satellite-packaging/compose && ./generate-compose.sh ${rhel} /home/jenkins/composes"
+ssh jenkins@$COMPOSE_HOST "cd satellite-packaging/compose && ./generate-compose.sh ${rhel} /home/jenkins"
