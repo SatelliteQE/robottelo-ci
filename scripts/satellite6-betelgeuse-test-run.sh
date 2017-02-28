196bdeac4091905ad8e2fb8a6e610dd1daa2ddde
Kedar Bidarkar <kbidarka@redhat.com>
2017-02-28 18:40:16 +0530

    Update how the custom-fields are specified.
    a) specify the custom-fields with space rather than '='
    b) The examples of betelgeuse suggests to use spaces.

--- scripts/satellite6-betelgeuse-test-run.sh
+++ scripts/satellite6-betelgeuse-test-run.sh
@@ -13,12 +13,12 @@ SANITIZED_ITERATION_ID="$(echo ${TEST_RUN_ID} | sed 's|\.|_|g' | sed 's| |_|g')"
 # Prepare the XML files
 for tier in $(seq 1 4); do
    for run in parallel sequential; do
-        betelgeuse --token-prefix="@" xml-test-run \
-            --custom-fields="isautomated=true" \
-            --custom-fields="arch=x8664" \
-            --custom-fields="variant=server" \
-            --custom-fields="plannedin=${SANITIZED_ITERATION_ID}" \
-            --response-property="${POLARION_SELECTOR}" \
+        betelgeuse --token-prefix @ xml-test-run \
+            --custom-fields isautomated=true \
+            --custom-fields arch=x8664 \
+            --custom-fields variant=server \
+            --custom-fields plannedin="${SANITIZED_ITERATION_ID}" \
+            --response-property "${POLARION_SELECTOR}" \
             --test-run-id "${TEST_RUN_ID} - ${run} - Tier ${tier}" \
             "./tier${tier}-${run}-results.xml" \
             tests/foreman \
