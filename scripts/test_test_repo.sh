815caacffee0b890b8f1eb4f0b9281087d579731
zjhuntin <zhunting@redhat.com>
2017-03-16 13:49:14 -0400

    Adjusting to properly use RVM

--- scripts/test_test_repo.sh
+++ scripts/test_test_repo.sh
@@ -1,9 +1,8 @@
-#!/bin/bash -ex
+#!/bin/bash -exl
 
 cd plugin/
 
 # RVM Ruby environment
-. /etc/profile.d/rvm.sh
 # Use a gemset unique to each executor to enable parallel builds
 gemset=$(echo ${JOB_NAME} | cut -d/ -f1)-${EXECUTOR_NUMBER}
 rvm use ruby-${ruby}@${gemset} --create
