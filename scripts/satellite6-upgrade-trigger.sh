5276f4a765c25b85833ac5e3e89580609f9e1d26
jyejare <jyejare@redhat.com>
2017-04-06 14:30:50 +0530

    Fix dosnstream Nightly Upgrade Trigger job

--- scripts/satellite6-upgrade-trigger.sh
+++ scripts/satellite6-upgrade-trigger.sh
@@ -7,12 +7,24 @@ export SATELLITE_VERSION="{satellite_version}"
 # Specify DISTRO as subscription_config file requires it.
 export DISTRO="${{OS}}"
 export OS_VERSION="${{OS: -1}}"
-export TO_VERSION="${{SATELLITE_VERSION}}"
+# TO_VERSION calculation according trigger type
+if [ "${{SATELLITE_VERSION}}" = 'downstream-nightly' ]; then
+    export TO_VERSION='6.3'
+else
+    export TO_VERSION="${{SATELLITE_VERSION}}"
+fi
+
+# FROM_VERSION calculation according to trigger type
 if [ "${{ZSTREAM_UPGRADE}}" = 'true' ]; then
     export FROM_VERSION="${{TO_VERSION}}"
 else
-    export FROM_VERSION=$(echo ${{SATELLITE_VERSION}} - 0.1 | bc)
+    if [ "${{SATELLITE_VERSION}}" = 'downstream-nightly' ]; then
+        export FROM_VERSION='6.2'
+    else
+        export FROM_VERSION=$(echo ${{SATELLITE_VERSION}} - 0.1 | bc)
+    fi
 fi
+
 export CLIENTS_COUNT=8
 
 # Sourcing and exporting required env vars
