ce3380e3474c10ba2ff5a04562400cc8374fc6a3
jyejare <jyejare@redhat.com>
2017-04-12 16:42:35 +0530

    Fix Upgrade Trigger Job with refactored automations tools code

--- scripts/satellite6-upgrade-trigger.sh
+++ scripts/satellite6-upgrade-trigger.sh
@@ -54,5 +54,10 @@ if [ "${{TO_VERSION}}" = '6.1' ]; then
     exit 1
 fi
 
+# Sets up the satellite, capsule and clients on rhevm or personal boxes before upgrading
+fab -u root setup_products_for_upgrade:'longrun',"{os}"
+
 # Longrun to run upgrade on Satellite, capsule and clients
 fab -u root product_upgrade:'longrun'
+
+
