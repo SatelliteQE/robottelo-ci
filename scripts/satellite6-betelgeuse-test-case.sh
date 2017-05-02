0a4a4c29d4f53452df8907adb4c66782ae23946c
Kedar Bidarkar <kbidarka@redhat.com>
2017-05-02 19:01:28 +0530

    betelgeuse version and commands depend upon satellite_version
    a) Depending upon the satellite_version the betelgeuse version is
       used for only satellite6-betelgeuse-test-run job.
    b) For satellite_version 6.1 and 6.2 the betelgeuse version is
       '<0.8'. The exact betelgeuse version would be 0.7.1.
    c) For satellite_version 6.3, downstream-nightly and
       upstream-nightly the betelgeuse version would be the latest.
    d) These changes would help using betelgeuse with --token-prefix
       command depending upon the satellite version.
    e) Also satellite6-betelgeuse.sh script is renamed as now it only
       does the installtion of pylarion. As at this stage the
       betelgeuse-test-case job has no way of knowing the
       satellite_version to install the correct betelgeuse version.
    f) Installing betelgeuse is now moved to at a later stage,
       as satellite_version is only required for the,
       satellite6-betelgeuse-test-run job and not for the,
       satellite6-betelgeuse-test-case job, which requires only the
       latest version of betelgeuse.

--- scripts/satellite6-betelgeuse-test-case.sh
+++ scripts/satellite6-betelgeuse-test-case.sh
@@ -1,3 +1,6 @@
+# Install the latest version of betelgeuse.
+pip install betelgeuse
+
 betelgeuse test-case \
     --path tests/foreman \
     --automation-script-format "https://github.com/SatelliteQE/robottelo/blob/master/{path}#L{line_number}" \
