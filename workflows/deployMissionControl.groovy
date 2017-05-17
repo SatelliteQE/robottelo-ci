19fc9430bf390f44ec6fcc0a0db21a815af2eb2e
Evgeni Golov <egolov@redhat.com>
2017-05-17 11:52:38 +0200

    don't fail if git remote rm fails, the remote might be missing

--- workflows/deployMissionControl.groovy
+++ workflows/deployMissionControl.groovy
@@ -3,7 +3,7 @@ node('sat6-rhel7') {
             
     git url: "https://${env.GIT_HOSTNAME}/satellite6/mission-control.git"
 
-    sh "git remote rm openshift"
+    sh "git remote rm openshift || true"
     sh "git remote add openshift ${env.MISSION_CONTROL_OPENSHIFT_GIT_REPO}"
     sh "git push openshift master:master --force"
 
