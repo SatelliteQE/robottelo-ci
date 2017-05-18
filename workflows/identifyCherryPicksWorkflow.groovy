2159b6d7ac52e6072972952f45adb3d3a36d0657
zjhuntin <zhunting@redhat.com>
2017-05-18 09:06:46 -0400

    Add octokit credentials to cherry-pick job

--- workflows/identifyCherryPicksWorkflow.groovy
+++ workflows/identifyCherryPicksWorkflow.groovy
@@ -1,7 +1,9 @@
 node('rhel') {
   stage "Identify Cherry Picks"
 
-  withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'bugzilla-credentials', passwordVariable: 'BZ_PASSWORD', usernameVariable: 'BZ_USERNAME']]) {
+  withCredentials([
+      [$class: 'UsernamePasswordMultiBinding', credentialsId: 'bugzilla-credentials', passwordVariable: 'BZ_PASSWORD', usernameVariable: 'BZ_USERNAME'],
+      [$class: 'UsernamePasswordMultiBinding', credentialsId: 'octokit_token', passwordVariable: 'OCTOKIT_ACCESS_TOKEN', usernameVariable: 'OCTOKIT_TOKEN']]) {
     git url: "https://${env.GIT_HOSTNAME}/Satellite6/tool_belt.git", branch: 'master'
     sh 'bundle install'
     sh "./tools.rb bugzilla cherry-pick --username ${env.BZ_USERNAME} --password ${env.BZ_PASSWORD} --version 6.2.0 --milestone 6.2.10"
