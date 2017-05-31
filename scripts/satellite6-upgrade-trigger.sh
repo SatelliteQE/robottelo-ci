837359cbdfae25ef85b77802299eaca572890331
san7ket <san7ketjagtap@gmail.com>
2017-05-31 10:43:57 +0530

    Fixes passing of FROM_VERSION to existence tests

--- scripts/satellite6-upgrade-trigger.sh
+++ scripts/satellite6-upgrade-trigger.sh
@@ -1,6 +1,30 @@
+# Fix variables
+export CLIENTS_COUNT=8
+export RUN_EXISTANCE_TESTS=true
+
+# Sourcing and exporting required env vars
+source "${{CONFIG_FILES}}"
+source config/compute_resources.conf
+source config/sat6_upgrade.conf
+source config/sat6_repos_urls.conf
+source config/subscription_config.conf
+source config/fake_manifest.conf
+
+# Fetching correct BASE_URL and CAPSULE_URL
+export BASE_URL="${{SATELLITE6_REPO}}"
+export CAPSULE_URL="${{CAPSULE_REPO}}"
+
+# Write a properties file to allow passing variables to T1 tests job.
+echo "SERVER_HOSTNAME=${{RHEV_SAT_HOST}}" > properties.txt
+echo "TOOLS_REPO=${{TOOLS_REPO}}" >> properties.txt
+echo "SUBNET=${{SUBNET}}" >> properties.txt
+echo "NETMASK=${{NETMASK}}" >> properties.txt
+echo "GATEWAY=${{GATEWAY}}" >> properties.txt
+echo "BRIDGE=${{BRIDGE}}" >> properties.txt
+echo "DISCOVERY_ISO=${{DISCOVERY_ISO}}" >> properties.txt
+
 # Setting Prerequisites
 pip install -r requirements.txt
-
 
 # Sets up the satellite, capsule and clients on rhevm or personal boxes before upgrading
 fab -u root setup_products_for_upgrade:'longrun',"{os}"
