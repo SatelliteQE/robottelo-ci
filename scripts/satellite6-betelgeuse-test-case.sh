8850481d5af3caa99fabc33c00022d938f8c8f0f
Elyézer Rezende <elyezermr@gmail.com>
2017-06-06 15:59:29 -0300

    Make polarion-test-run use test case xml importer
    Use Test Case XML importer to create/update the test cases on Polarion.
    Use the CI Subscriber to wait for the import message.

--- scripts/satellite6-betelgeuse-test-case.sh
+++ scripts/satellite6-betelgeuse-test-case.sh
@@ -1,8 +1,14 @@
 # Install the latest version of betelgeuse.
 pip install betelgeuse
 
-betelgeuse test-case \
-    --path "${BETELGEUSE_TC_PATH}" \
+betelgeuse xml-test-case \
+    --response-property "${BETELGEUSE_RESPONSE_PROPERTY}" \
     --automation-script-format "https://github.com/SatelliteQE/${BETELGEUSE_AUTOMATION_PROJECT}/blob/master/{path}#L{line_number}" \
-    "${POLARION_PROJECT}"
+    "${BETELGEUSE_TC_PATH}" \
+    "${POLARION_PROJECT}" \
+    polarion-test-cases.xml
 
+curl -k -u "${POLARION_USERNAME}:${POLARION_PASSWORD}" \
+    -X POST \
+    -F file=@polarion-test-cases.xml \
+    "${POLARION_URL}import/testcase"
