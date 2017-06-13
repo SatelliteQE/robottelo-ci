c7700dbf4ea287fb710071b06bef39d165cfd53c
Eric D Helms <ericdhelms@gmail.com>
2017-06-13 11:45:30 -0400

    Rename Mission Control to Ohsnap

---
+++ workflows/deployOhsnap.groovy
@@ -0,0 +1,12 @@
+node('sat6-rhel7') {
+
+    stage "Push to Openshift" {
+
+        git url: "https://${env.GIT_HOSTNAME}/satellite6/ohsnap.git"
+
+        sh "git remote rm openshift || true"
+        sh "git remote add openshift ${env.OHSNAP_OPENSHIFT_GIT_REPO}"
+        sh "git push openshift master:master --force"
+
+    }
+}
