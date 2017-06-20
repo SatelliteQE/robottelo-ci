8df984b1814a54355801fc94995eff448345a1cb
Evgeni Golov <egolov@redhat.com>
2017-06-20 13:58:19 +0200

    deploy Ohsnap to Open Platform

--- workflows/deployOhsnap.groovy
+++ workflows/deployOhsnap.groovy
@@ -9,4 +9,10 @@ node('sat6-rhel7') {
         sh "git push openshift master:master --force"
 
     }
+
+    stage("Push to Open Platform") {
+        withCredentials([string(credentialsId: 'OHSNAP_OPEN_PAAS_WEBHOOK_URL', variable: 'OHSNAP_OPEN_PAAS_WEBHOOK_URL')]) {
+            sh "curl -k -XPOST ${OHSNAP_OPEN_PAAS_WEBHOOK_URL}"
+	}
+    }
 }
