9215e15c633da4c43ac70a41c63c720261402e4b
Eric D Helms <ericdhelms@gmail.com>
2017-06-26 15:23:41 -0400

    Switch 6.3 to use build_compose playbook

---
+++ workflows/6.3/releaseSatellitePipeline.groovy
@@ -0,0 +1,100 @@
+node('sat6-rhel7') {
+
+    stage("Setup Workspace") {
+
+        setupAnsibleEnvironment()
+
+    }
+
+    stage("Generate Composes") {
+
+        def compose_git_repo = env.COMPOSE_GIT_REPOSITORY ?: ''
+
+        runPlaybook {
+            playbook = 'playbooks/clone_compose_repo.yml'
+            extraVars = [
+                'compose_git_repo': compose_git_repo
+            ]
+        }
+
+        runPlaybookInParallel {
+            name = "compose-rhel"
+            items = compose_versions
+            item_name = 'rhel_version'
+            playbook = 'playbooks/build_compose.yml'
+            extraVars = [
+                'compose_git_repo': compose_git_repo,
+                'compose_version': 'satellite-6.3',
+                'compose_label': 'Satellite-6.3',
+                'compose_name': 'satellite-6.3-rhel-7'
+            ]
+        }
+
+        runPlaybookInParallel {
+            name = "compose-tools-rhel"
+            items = tools_compose_versions
+            item_name = 'rhel_version'
+            playbook = 'playbooks/build_compose.yml'
+            extraVars = [
+                'compose_git_repo': compose_git_repo,
+                'compose_version': 'satellite-6.3',
+                'compose_label': 'SatTools-6.3',
+                'compose_name': 'satellite-tools-6.3-rhel-7'
+            ]
+        }
+
+    }
+
+    stage("Test Installation") {
+
+        runOnLibvirtHost "cd sat-deploy && git -c http.sslVerify=false fetch origin && git reset origin/master --hard"
+        runOnLibvirtHost "cd sat-deploy/forklift && git -c http.sslVerify=false fetch origin && git reset origin/master --hard"
+
+        def branches = [:]
+
+        wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "xterm"]) {
+            for (int i = 0; i < os_versions.size(); i++) {
+                def index = i // fresh variable per iteration; i will be mutated
+                def item = os_versions.get(index)
+
+                branches["install-rhel-${item}"] = {
+                    try {
+                        runOnLibvirtHost "cd sat-deploy && ansible-playbook pipelines/compose_test_${satellite_short_version}_rhel${item}.yml -e forklift_state=up"
+                    } finally {
+                        runOnLibvirtHost "cd sat-deploy && ansible-playbook pipelines/compose_test_${satellite_short_version}_rhel${item}.yml -e forklift_state=destroy"
+                    }
+                }
+            }
+        }
+
+        parallel branches
+
+    }
+
+
+    stage("Sync Repositories") {
+
+        runPlaybookInParallel {
+            name = "sync"
+            items = products
+            item_name = 'product'
+            playbook = 'playbooks/sync_repositories.yml'
+        }
+
+    }
+
+    stage("Publish Content Views") {
+
+        runPlaybookInParallel {
+            name = "publish"
+            items = content_views
+            item_name = 'content_view'
+            playbook = 'playbooks/publish_content_views.yml'
+        }
+
+    }
+}
+
+def runOnLibvirtHost(action) {
+    sh "ssh jenkins@${env.LIBVIRT_HOST} \"${action}\""
+}
---
+++ workflows/nightly/releaseSatellitePipeline.groovy
@@ -0,0 +1,83 @@
+node('sat6-rhel7') {
+
+    stage("Setup Workspace") {
+
+        setupAnsibleEnvironment()
+
+    }
+
+    stage("Generate Composes") {
+
+        def compose_git_repo = env.COMPOSE_GIT_REPOSITORY ?: ''
+
+        runPlaybook {
+            playbook = 'playbooks/update_packaging_repo.yml'
+            extraVars = [
+                'git_server': git_hostname,
+                'git_group': 'satellite6',
+                'satellite_version': satellite_version
+            ]
+        }
+
+        runPlaybookInParallel {
+            name = "compose-rhel"
+            items = compose_versions
+            item_name = 'rhel_version'
+            playbook = 'playbooks/generate_compose.yml'
+        }
+
+    }
+
+    stage("Test Installation") {
+
+        runOnLibvirtHost "cd sat-deploy && git -c http.sslVerify=false fetch origin && git reset origin/master --hard"
+        runOnLibvirtHost "cd sat-deploy/forklift && git -c http.sslVerify=false fetch origin && git reset origin/master --hard"
+
+        def branches = [:]
+
+        wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "xterm"]) {
+            for (int i = 0; i < os_versions.size(); i++) {
+                def index = i // fresh variable per iteration; i will be mutated
+                def item = os_versions.get(index)
+
+                branches["install-rhel-${item}"] = {
+                    try {
+                        runOnLibvirtHost "cd sat-deploy && ansible-playbook pipelines/compose_test_${satellite_short_version}_rhel${item}.yml -e forklift_state=up"
+                    } finally {
+                        runOnLibvirtHost "cd sat-deploy && ansible-playbook pipelines/compose_test_${satellite_short_version}_rhel${item}.yml -e forklift_state=destroy"
+                    }
+                }
+            }
+        }
+
+        parallel branches
+
+    }
+
+
+    stage("Sync Repositories") {
+
+        runPlaybookInParallel {
+            name = "sync"
+            items = products
+            item_name = 'product'
+            playbook = 'playbooks/sync_repositories.yml'
+        }
+
+    }
+
+    stage("Publish Content Views") {
+
+        runPlaybookInParallel {
+            name = "publish"
+            items = content_views
+            item_name = 'content_view'
+            playbook = 'playbooks/publish_content_views.yml'
+        }
+
+    }
+}
+
+def runOnLibvirtHost(action) {
+    sh "ssh jenkins@${env.LIBVIRT_HOST} \"${action}\""
+}
