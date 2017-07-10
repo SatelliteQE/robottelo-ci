9180c80b022f7365a530ceba56cf6d70925d3011
Lukas Pramuk <lpramuk@redhat.com>
2017-07-10 13:40:25 +0200

    Use -D (--disable-known-hosts) with fab commands

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -1,7 +1,7 @@
 pip install -U -r requirements.txt
 
 # Figure out what version of RHEL the server uses
-export OS_VERSION=$(fab -H root@${SERVER_HOSTNAME} distro_info | grep "rhel [[:digit:]]" | cut -d ' ' -f 2)
+export OS_VERSION=$(fab -D -H root@${SERVER_HOSTNAME} distro_info | grep "rhel [[:digit:]]" | cut -d ' ' -f 2)
 DISTRO="rhel${OS_VERSION}"
 
 source ${CONFIG_FILES}
@@ -23,11 +23,11 @@ export EXTERNAL_AUTH
 export IDM_REALM
 
 if [ ${FIX_HOSTNAME} = "true" ]; then
-    fab -H root@${SERVER_HOSTNAME} fix_hostname
+    fab -D -H root@${SERVER_HOSTNAME} fix_hostname
 fi
 
 if [ ${PARTITION_DISK} = "true" ]; then
-    fab -H root@${SERVER_HOSTNAME} partition_disk
+    fab -D -H root@${SERVER_HOSTNAME} partition_disk
 fi
 
 # Assign DISTRIBUTION to trigger things appropriately from automation-tools.
@@ -67,9 +67,9 @@ if [ "${SATELLITE_DISTRIBUTION}" = "INTERNAL" ]; then
     fi
 fi
 
-fab -H root@${SERVER_HOSTNAME} product_install:${DISTRIBUTION},sat_cdn_version=${SATELLITE_VERSION},test_in_stage=${STAGE_TEST}
+fab -D -H root@${SERVER_HOSTNAME} product_install:${DISTRIBUTION},sat_cdn_version=${SATELLITE_VERSION},test_in_stage=${STAGE_TEST}
 
 if [ ${SETUP_FAKE_MANIFEST_CERTIFICATE} = "true" ]; then
     source config/fake_manifest.conf
-    fab -H root@${SERVER_HOSTNAME} setup_fake_manifest_certificate
+    fab -D -H root@${SERVER_HOSTNAME} setup_fake_manifest_certificate
 fi
