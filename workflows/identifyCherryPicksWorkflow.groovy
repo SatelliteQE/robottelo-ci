399831a2fb8bcc1232fa060653d0a4bb2bf57f88
Evgeni Golov <evgeni@redhat.com>
2017-07-14 15:40:32 +0200

    split cherry-picks into setup and pick steps

--- workflows/identifyCherryPicksWorkflow.groovy
+++ workflows/identifyCherryPicksWorkflow.groovy
@@ -1,13 +1,17 @@
 node('rhel') {
 
+    stage("Setup ToolBelt") {
+        git url: "https://${env.GIT_HOSTNAME}/Satellite6/tool_belt.git", branch: 'master'
+        sh 'bundle install'
+        sh './tools.rb setup-environment --bugzilla --gitlab-username jenkins --version 6.2.0'
+    }
+
     stage("Identify Cherry Picks") {
 
         withCredentials([
             [$class: 'UsernamePasswordMultiBinding', credentialsId: 'bugzilla-credentials', passwordVariable: 'BZ_PASSWORD', usernameVariable: 'BZ_USERNAME'],
             [$class: 'UsernamePasswordMultiBinding', credentialsId: 'octokit_token', passwordVariable: 'OCTOKIT_ACCESS_TOKEN', usernameVariable: 'OCTOKIT_TOKEN']]) {
 
-                git url: "https://${env.GIT_HOSTNAME}/Satellite6/tool_belt.git", branch: 'master'
-                sh 'bundle install'
                 sh "./tools.rb bugzilla cherry-pick --username ${env.BZ_USERNAME} --password ${env.BZ_PASSWORD} --version 6.2.0 --milestone 6.2.11"
                 archive "releases/6.2.0/bugzillas"
           }
