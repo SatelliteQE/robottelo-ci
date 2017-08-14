270f88e85357479c7a9d02d0376aadecf469f39f
Evgeni Golov <evgeni@redhat.com>
2017-08-14 08:55:46 +0200

    update cherry-pick job for 6.2.12
    also make it easier to configure

--- workflows/identifyCherryPicksWorkflow.groovy
+++ workflows/identifyCherryPicksWorkflow.groovy
@@ -1,9 +1,12 @@
+def version = '6.2.0'
+def milestone = '6.2.12'
+
 node('rhel') {
 
     stage("Setup ToolBelt") {
         git url: "https://${env.GIT_HOSTNAME}/Satellite6/tool_belt.git", branch: 'master'
         sh 'bundle install'
-        sh './tools.rb setup-environment --bugzilla --gitlab-username jenkins --version 6.2.0'
+        sh "./tools.rb setup-environment --bugzilla --gitlab-username jenkins --version ${version}"
     }
 
     stage("Identify Cherry Picks") {
@@ -12,8 +15,8 @@ node('rhel') {
             [$class: 'UsernamePasswordMultiBinding', credentialsId: 'bugzilla-credentials', passwordVariable: 'BZ_PASSWORD', usernameVariable: 'BZ_USERNAME'],
             [$class: 'UsernamePasswordMultiBinding', credentialsId: 'octokit_token', passwordVariable: 'OCTOKIT_ACCESS_TOKEN', usernameVariable: 'OCTOKIT_TOKEN']]) {
 
-                sh "./tools.rb bugzilla cherry-pick --username ${env.BZ_USERNAME} --password ${env.BZ_PASSWORD} --version 6.2.0 --milestone 6.2.11 --no-update-repos"
-                archive "releases/6.2.0/bugzillas"
+                sh "./tools.rb bugzilla cherry-pick --username ${env.BZ_USERNAME} --password ${env.BZ_PASSWORD} --version ${version} --milestone ${milestone} --no-update-repos"
+                archive "releases/${version}/bugzillas"
           }
     }
 
