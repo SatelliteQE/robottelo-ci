df261a22a8a30a80757c16c7449f1c3112daa84d
Kedar Bidarkar <kbidarka@dhcp223-54.pnq.redhat.com>
2017-08-28 00:44:36 +0530

    Use -D with fab command which is disabling known-hosts.

--- scripts/satellite6-manifest-downloader.sh
+++ scripts/satellite6-manifest-downloader.sh
@@ -2,4 +2,4 @@ pip install -r requirements.txt
 source ${CONFIG_FILES}
 source config/fake_manifest.conf
 source config/subscription_config.conf
-fab -H "root@${MANIFEST_SERVER_HOSTNAME}" relink_manifest
+fab -D -H "root@${MANIFEST_SERVER_HOSTNAME}" relink_manifest
