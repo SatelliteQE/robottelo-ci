336d2203123a16348b483dd86a54adf8300fe3ff
Eric D Helms <ericdhelms@gmail.com>
2017-09-06 18:33:56 -0500

    Checkout foreman_docker and bastion 6.2.0 only

--- scripts/add_foreman_docker_gitlab.sh
+++ scripts/add_foreman_docker_gitlab.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-pushd foreman
-echo "gem 'foreman_docker', :git => 'https://${GIT_HOSTNAME}/${GIT_ORGANIZATION}/foreman_docker.git', :branch => '${gitlabTargetBranch}'" >> bundler.d/Gemfile.local.rb
-popd foreman
+if [[ $gitlabTargetBranch == 'SATELLITE-6.2.0' ]];then
+  pushd foreman
+  echo "gem 'foreman_docker', :git => 'https://${GIT_HOSTNAME}/${GIT_ORGANIZATION}/foreman_docker.git', :branch => '${gitlabTargetBranch}'" >> bundler.d/Gemfile.local.rb
+  popd foreman
+fi
