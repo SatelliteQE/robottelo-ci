ce5fb5e0843c7e0c7187fdfdf59b07f7ff629815
Eric D Helms <ericdhelms@gmail.com>
2017-09-08 08:54:34 -0700

    Add katello-installer unit test

---
+++ workflows/test/katelloInstaller.groovy
@@ -0,0 +1,27 @@
+node('rhel') {
+
+    snapperStage("Setup Environment") {
+
+        deleteDir()
+        gitlab_clone_and_merge('katello-installer')
+
+    }
+
+    snapperStage('Tests') {
+
+        try {
+
+            withRVM(["gem install bundler"])
+            withRVM(["bundle install"])
+            withRVM(["FOREMAN_BRANCH=1.15-stable bundle exec rake"])
+
+        } finally {
+
+            archive "pkg/*"
+            cleanup_rvm()
+
+        }
+
+    }
+
+}
