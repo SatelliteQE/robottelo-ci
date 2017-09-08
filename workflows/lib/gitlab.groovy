449a0b8dc2dda2045f0141207d5aea9b5cfe63f2
Evgeni Golov <evgeni@redhat.com>
2017-09-08 12:55:25 +0200

    use the GitSCM module to merge git stuff, not sh
    also move the code into an own function under lib

---
+++ workflows/lib/gitlab.groovy
@@ -0,0 +1,14 @@
+def gitlab_clone_and_merge(repo_name) {
+    if (env.gitlabSourceRepoName) {
+        checkout changelog: true, poll: true, scm: [
+            $class: 'GitSCM',
+            branches: [[name: "pr/${env.gitlabSourceBranch}"]],
+            doGenerateSubmoduleConfigurations: false,
+            extensions: [[$class: 'PreBuildMerge', options: [fastForwardMode: 'FF', mergeRemote: 'origin', mergeStrategy: 'default', mergeTarget: "${env.gitlabTargetBranch}"]]],
+            submoduleCfg: [],
+            userRemoteConfigs: [[name: 'origin', url: "https://$GIT_HOSTNAME/$GIT_ORGANIZATION/${repo_name}.git"], [name: 'pr', url: "https://$GIT_HOSTNAME/${env.gitlabSourceRepoName}.git"]]
+          ]
+    } else {
+        git url: "https://$GIT_HOSTNAME/$GIT_ORGANIZATION/${repo_name}.git", branch: gitlabTargetBranch
+    }
+}
