83fa9bbf27692cf56aad811833e863a067b7506c
Eric D Helms <ericdhelms@gmail.com>
2017-09-21 08:35:57 -0400

    Restrict puppet unittest job matrices based on Satellite version

--- workflows/lib/rvm.groovy
+++ workflows/lib/rvm.groovy
@@ -1,18 +1,25 @@
-def gemset() {
-    "${JOB_NAME}-${BUILD_ID}"
+def gemset(name) {
+
+    def base_name = "${JOB_NAME}-${BUILD_ID}"
+
+    if (name) {
+        base_name = base_name + '-' + name
+    }
+
+    base_name
 }
 
-def cleanup_rvm() {
-    withRVM(["rvm gemset delete ${gemset()} --force"])
+def cleanup_rvm(name = '') {
+    withRVM(["rvm gemset delete ${gemset(name)} --force"])
 }
 
-def withRVM(commands, ruby = '2.0') {
+def withRVM(commands, ruby = '2.0', name = '') {
 
     commands = commands.join("\n")
     echo commands
 
     sh """#!/bin/bash -l
-        rvm use ruby-${ruby}@${gemset()} --create
+        rvm use ruby-${ruby}@${gemset(name)} --create
         ${commands}
     """
 }
---
+++ workflows/test/puppet.groovy
@@ -0,0 +1,46 @@
+node('rhel') {
+
+    def tests = [:]
+    def puppet_ruby = [
+        [puppet_version: '3.8', ruby_version: '1.8.7', satellite_versions: ['6.2.0']],
+        [puppet_version: '3.8', ruby_version: '2.0.0', satellite_versions: ['6.2.0', '6.3.0']],
+        [puppet_version: '4.10.7', ruby_version: '2.1.9', satellite_versions: ['6.3.0']]
+    ]
+
+    for (combo in puppet_ruby) {
+        if (!combo['satellite_versions'].contains(gitlabTargetBranch.minus('SATELLITE-'))) {
+            continue
+        }
+
+        def name = "puppet-${combo['puppet_version']}_ruby-${combo['ruby_version']}"
+
+        tests[name] = {
+            node('rhel') {
+
+                try {
+
+                    wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "xterm"]) {
+                        deleteDir()
+                        gitlab_clone_and_merge(puppet_repo)
+
+                        withRVM(["gem install bundler"], combo['ruby_version'], name)
+                        withRVM(["PUPPET_VERSION=${combo['puppet_version']} bundle install --without system_tests development"], combo['ruby_version'], name)
+                        withRVM(["ONLY_OS=redhat-6-x86_64,redhat-7-x86_64 bundle exec rake"], combo['ruby_version'], name)
+                    }
+
+                } finally {
+
+                    cleanup_rvm(name)
+
+                }
+            }
+        }
+    }
+
+    snapperStage('Run Tests') {
+
+        parallel tests
+
+    }
+
+}
