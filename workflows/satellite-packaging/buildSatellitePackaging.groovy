bbf74637766d73249d7955534ff1fc0c36a70a39
Evgeni Golov <evgeni@redhat.com>
2017-10-09 09:37:41 +0200

    release packages into brew on merge to satellite-packaging

--- workflows/satellite-packaging/buildSatellitePackaging.groovy
+++ workflows/satellite-packaging/buildSatellitePackaging.groovy
@@ -9,8 +9,15 @@ node('sat6-rhel7') {
     stage("Find packages to build") {
         if (project) {
             packages_to_build = project
-        } else {
-            changed_packages = sh(returnStdout: true, script: "git diff ..origin/${env.gitlabTargetBranch} --name-only packages/ | cut -d'/' -f2 |sort -u").trim()
+        } else if (env.releaseBranch) {
+            merge_info = sh(returnStdout: true, script: "git rev-list --parents -n 1  origin/${env.releaseBranch}").split()
+            // check if we got two parents, otherwise it's not a merge
+            if (merge_info.length == 3) {
+                changed_packages = sh(returnStdout: true, script: "git diff ${merge_info[1]}...${merge_info[2]} --name-only 'packages/*.spec' | cut -d'/' -f2 |sort -u").trim()
+                packages_to_build = changed_packages.split().join(':')
+            }
+        } else if (env.gitlabTargetBranch) {
+            changed_packages = sh(returnStdout: true, script: "git diff ..origin/${env.gitlabTargetBranch} --name-only 'packages/*.spec' | cut -d'/' -f2 |sort -u").trim()
             packages_to_build = changed_packages.split().join(':')
         }
         if (!packages_to_build) {
