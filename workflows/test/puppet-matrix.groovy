10cceef96183aea980949ca274af84de98fe38e3
Evgeni Golov <evgeni@golov.de>
2017-11-15 16:02:56 +0100

    rewrite smart-proxy tests as pipelines (#882)

---
+++ workflows/test/puppet-matrix.groovy
@@ -0,0 +1,5 @@
+def puppet_ruby = [
+    [puppet_version: '3.8', ruby_version: '1.8.7', satellite_versions: ['6.2.0']],
+    [puppet_version: '3.8', ruby_version: '2.0.0', satellite_versions: ['6.2.0', '6.3.0']],
+    [puppet_version: '4.10.7', ruby_version: '2.1.9', satellite_versions: ['6.3.0']]
+]
