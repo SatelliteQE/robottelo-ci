1357416e69fd9c4535fc28ccfd05002b5ac29dfa
Zach Huntington-Meath <zhunting@redhat.com>
2017-12-05 17:41:42 -0500

    Remove compare content view logic in favor of tool_belt and ohsnap

--- workflows/lib/releaseQAFunctions.groovy
+++ workflows/lib/releaseQAFunctions.groovy
@@ -18,72 +18,6 @@ def promoteContentView(body) {
     }
 }
 
-def findContentView(body) {
-
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-
-    withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'artefact-satellite-credentials', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME']]) {
-
-        def cmd = [
-          "/bin/bash --login -c",
-          "'rvm system && ",
-          "hammer --output json --username \"${env.USERNAME}\" --password \"${env.PASSWORD}\" --server ${env.SATELLITE_SERVER}",
-          "content-view version list",
-          "--organization \"${config.organization}\"",
-          "--environment \"${config.lifecycle_environment}\"",
-          "--content-view \"${config.content_view}\"'"
-        ]
-
-        sh "${cmd.join(' ')} > versions.json"
-
-        def versions = readFile "versions.json"
-        versions = new JsonSlurper().parseText(versions)
-
-        if (versions.size() == 0) {
-            return null;
-        } else {
-            return versions.first()['ID'];
-        }
-    }
-}
-
-def computePackageDifference(body) {
-
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-
-    withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'artefact-satellite-credentials', passwordVariable: 'SATELLITE_PASSWORD', usernameVariable: 'SATELLITE_USERNAME']]) {
-
-        dir('tool_belt') {
-
-            setup_toolbelt()
-            def archive_file = 'package_report.yaml'
-
-            def cmd = [
-                "bundle exec",
-                "./tools.rb release compare-content-view",
-                "--server '${env.SATELLITE_SERVER}'",
-                "--username '${env.SATELLITE_USERNAME}' --password '${env.SATELLITE_PASSWORD}'",
-                "--organization '${config.organization}'",
-                "--content-view '${config.content_view}'",
-                "--from-environment '${config.from_environment}'",
-                "--to-environment '${config.to_environment}'",
-                "--output '${archive_file}'"
-            ]
-
-            sh "${cmd.join(' ')}"
-            archive archive_file
-
-        }
-    }
-
-}
-
 def createLifecycleEnvironment(body) {
 
     def config = [:]
@@ -108,33 +42,24 @@ def compareContentViews(body) {
     body.delegate = config
     body()
 
-    def versionInTest = findContentView {
-      organization = config.organization
-      content_view = config.content_view
-      lifecycle_environment = config.from_lifecycle_environment
-    }
+    withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'artefact-satellite-credentials', passwordVariable: 'SATELLITE_PASSWORD', usernameVariable: 'SATELLITE_USERNAME']]) {
 
-    def versionInQA = findContentView {
-      organization = config.organization
-      content_view = config.content_view
-      lifecycle_environment = config.to_lifecycle_environment
-    }
+        dir('tool_belt') {
 
-    echo versionInTest.toString()
-    echo versionInQA.toString()
+            setup_toolbelt()
+            def archive_file = 'package_report.yaml'
 
-    if (versionInTest != versionInQA && versionInTest != null) {
+            def cmd = [
+                "bundle exec",
+                "./tools.rb release compare-content-view",
+                "--content-view '${config.content_view}'",
+                "--from-environment '${config.from_environment}'",
+                "--to-environment '${config.to_environment}'",
+                "--output '${archive_file}'"
+            ]
 
-        computePackageDifference {
-          organization = config.organization
-          content_view = config.content_view
-          from_environment = config.from_lifecycle_environment
-          to_environment = config.to_lifecycle_environment
+            sh "${cmd.join(' ')}"
+            archive archive_file
         }
-
-    } else {
-
-        echo "Version already promoted, no package changes calculated"
-
     }
 }
