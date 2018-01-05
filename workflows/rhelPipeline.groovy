dcc03b2ec03ee4d1a777bcfb8a5ab68650a6401d
Eric D Helms <ericdhelms@gmail.com>
2018-01-05 09:30:58 -0500

    Add pipeline to sync and publish RHEL weekly

---
+++ workflows/rhelPipeline.groovy
@@ -0,0 +1,39 @@
+import groovy.json.JsonSlurper
+
+node('rhel') {
+
+    snapperStage("Setup Workspace") {
+
+        deleteDir()
+        setupAnsibleEnvironment {}
+
+    }
+
+    snapperStage("Sync Repositories") {
+        def products = [
+            'Red Hat Enterprise Linux Server',
+            'Red Hat Software Collections for RHEL Server'
+        ]
+
+        runPlaybookInParallel {
+            name = "sync"
+            items = products
+            item_name = 'product'
+            playbook = 'playbooks/sync_repositories.yml'
+        }
+    }
+
+    snapperStage("Publish Content Views") {
+        def content_views = [
+            'RHEL6',
+            'RHEL7'
+        ]
+
+        runPlaybookInParallel {
+            name = "publish"
+            items = content_views
+            item_name = 'content_view'
+            playbook = 'playbooks/publish_content_views.yml'
+        }
+    }
+}
