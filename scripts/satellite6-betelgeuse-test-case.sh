9bd59cc8e2fe9e5e698723e8b5869f01e2228889
Kedar Bidarkar <kbidarka@redhat.com>
2018-01-18 15:03:39 -0500

    Update the betelgeuse/polarion testcase importer into smaller chunks
    Signed-off-by: Kedar Bidarkar <kbidarka@redhat.com>

--- scripts/satellite6-betelgeuse-test-case.sh
+++ scripts/satellite6-betelgeuse-test-case.sh
@@ -1,9 +1,10 @@
 # Install the latest version of betelgeuse.
 pip install betelgeuse
 
+for TC_PATH in "${BETELGEUSE_TC_PATH}" ; do \
 betelgeuse requirement \
-    "${BETELGEUSE_TC_PATH}" \
-    "${POLARION_PROJECT}"
+    "${TC_PATH}" \
+    "${POLARION_PROJECT}" ; done
 
 export PYTHONPATH="${PWD}"
 
@@ -13,12 +14,13 @@ DEFAULT_STATUS_VALUE = 'approved'
 DEFAULT_SUBTYPE2_VALUE = '-'
 EOF
 
+for TC_PATH in "${BETELGEUSE_TC_PATH}" ; do \
 betelgeuse --config-module "betelgeuse_config" test-case \
     --response-property "${BETELGEUSE_RESPONSE_PROPERTY}" \
     --automation-script-format "https://github.com/SatelliteQE/${BETELGEUSE_AUTOMATION_PROJECT}/blob/master/{path}#L{line_number}" \
-    "${BETELGEUSE_TC_PATH}" \
+    "${TC_PATH}" \
     "${POLARION_PROJECT}" \
-    polarion-test-cases.xml
+    polarion-test-cases.xml ; done
 
 curl -k -u "${POLARION_USERNAME}:${POLARION_PASSWORD}" \
     -X POST \
