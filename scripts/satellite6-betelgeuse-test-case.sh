f9448e2741f2005f53afa08f8cd9859c86080e61
Kedar Bidarkar <kbidarka@redhat.com>
2018-01-18 19:52:33 -0500

    Fixed the polarion importer to upload in smaller chunks
    Signed-off-by: Kedar Bidarkar <kbidarka@redhat.com>

--- scripts/satellite6-betelgeuse-test-case.sh
+++ scripts/satellite6-betelgeuse-test-case.sh
@@ -1,9 +1,9 @@
 # Install the latest version of betelgeuse.
 pip install betelgeuse
 
-for TC_PATH in "${BETELGEUSE_TC_PATH}" ; do \
+for TC_PATH in $(echo ${BETELGEUSE_TC_PATH}) ; do \
 betelgeuse requirement \
-    "${TC_PATH}" \
+    ${TC_PATH} \
     "${POLARION_PROJECT}" ; done
 
 export PYTHONPATH="${PWD}"
@@ -14,15 +14,14 @@ DEFAULT_STATUS_VALUE = 'approved'
 DEFAULT_SUBTYPE2_VALUE = '-'
 EOF
 
-for TC_PATH in "${BETELGEUSE_TC_PATH}" ; do \
+for TC_PATH in $(echo ${BETELGEUSE_TC_PATH}) ; do \
 betelgeuse --config-module "betelgeuse_config" test-case \
     --response-property "${BETELGEUSE_RESPONSE_PROPERTY}" \
     --automation-script-format "https://github.com/SatelliteQE/${BETELGEUSE_AUTOMATION_PROJECT}/blob/master/{path}#L{line_number}" \
-    "${TC_PATH}" \
+    ${TC_PATH} \
     "${POLARION_PROJECT}" \
-    polarion-test-cases.xml ; done
-
+    polarion-test-cases.xml ; \
 curl -k -u "${POLARION_USERNAME}:${POLARION_PASSWORD}" \
     -X POST \
     -F file=@polarion-test-cases.xml \
-    "${POLARION_URL}import/testcase"
+    "${POLARION_URL}import/testcase" ; done
