0d18885aef5c9faddaa19193123cb748f69a9a7b
Eric D Helms <ericdhelms@gmail.com>
2018-01-19 08:53:36 -0500

    Switch to syncing repositories instead of products for 6.3 pipeline

--- workflows/6.3/releasePipelineAttributes.groovy
+++ workflows/6.3/releasePipelineAttributes.groovy
@@ -6,10 +6,34 @@ def os_versions = ['7']
 def satellite_short_version = 63
 def satellite_version = '6.3.0'
 
-def products = [
-    'Satellite 6.3 Composes',
-    'Satellite Capsule 6.3 Composes',
-    'Satellite Tools 6.3 Composes'
+def satellite_repositories = [
+    "Satellite 6.3 RHEL7",
+    "Satellite 6.3 Puppet 4 RHEL7"
+]
+
+def capsule_repositories = [
+    "Satellite Capsule 6.3 RHEL7",
+    "Satellite Capsule 6.3 Puppet 4 RHEL7"
+]
+
+def tools_repositories = [
+    "Satellite Tools 6.3 RHEL7 x86_64",
+    "Satellite Tools 6.3 RHEL7 ppc64le",
+    "Satellite Tools 6.3 RHEL7 ppc64",
+    "Satellite Tools 6.3 RHEL7 s390x",
+    "Satellite Tools 6.3 RHEL7 aarch64",
+    "Satellite Tools 6.3 RHEL6 x86_64",
+    "Satellite Tools 6.3 RHEL6 i386",
+    "Satellite Tools 6.3 RHEL6 ppc64",
+    "Satellite Tools 6.3 RHEL6 s390x",
+    "Satellite Tools 6.3 RHEL5 x86_64",
+    "Satellite Tools 6.3 RHEL5 i386",
+    "Satellite Tools 6.3 RHEL5 s390x",
+    "Satellite Tools Puppet 4 6.3 RHEL7 x86_64",
+    "Satellite Tools Puppet 4 6.3 RHEL6 x86_64",
+    "Satellite Tools Puppet 4 6.3 RHEL6 i386",
+    "Satellite Tools Puppet 4 6.3 RHEL5 x86_64",
+    "Satellite Tools Puppet 4 6.3 RHEL5 i386",
 ]
 
 def content_views = [
