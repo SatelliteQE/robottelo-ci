24e287bb8ae6949cc5e94cce86f08ef35d3ea343
Evgeni Golov <evgeni@golov.de>
2018-01-26 16:27:15 +0100

    use obal for satellite-packaging-update (#974)

--- workflows/lib/obal.groovy
+++ workflows/lib/obal.groovy
@@ -6,12 +6,17 @@ def obal(body) {
     body()
 
     def tags = config.tags ? "--tags ${config.tags}" : ""
+    def extra_vars = config.extraVars ?: [:]
+    def extra_vars_args = []
+    extra_vars.each { key, val ->
+       extra_vars_args += $/-e '${key}="${val}"'/$
+    }
 
     dir('obal') {
         git url: "https://github.com/evgeni/obal.git", branch: "master"
     }
 
     wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "xterm"]) {
-        sh "ANSIBLE_FORCE_COLOR=true PYTHONPATH=obal/ python -m obal.__init__ ${tags} ${config.action} ${config.packages}"
+        sh "ANSIBLE_FORCE_COLOR=true PYTHONPATH=obal/ python -m obal.__init__ ${tags} ${extra_vars_args.join(' ')} ${config.action} ${config.packages}"
     }
 }
