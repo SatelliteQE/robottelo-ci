4e9fb85d17e4a6da497ad1fa003286dda2c1f0ff
Patrick Creech <pcreech@redhat.com>
2018-02-16 14:40:52 -0500

    Fix typos in releasePackageWorkflow

--- workflows/releasePackageWorkflow.groovy
+++ workflows/releasePackageWorkflow.groovy
@@ -2,13 +2,13 @@ def branch_map = [
     'SATELLITE-6.2.0': [
         'repo': 'Satellite 6.2 Source Files',
         'version': '6.2.0',
-        'tool_belt_config': './configs/satellite/'
+        'tool_belt_config': './configs/satellite/',
         'foreman_branch': '1.11-stable'
     ],
     'SATELLITE-6.3.0': [
         'repo': 'Satellite 6.3 Source Files',
         'version': '6.3.0',
-        'tool_belt_config': './configs/satellite/'
+        'tool_belt_config': './configs/satellite/',
         'foreman_branch': '1.15-stable'
     ]
     'RHUI-3.0.0': [
