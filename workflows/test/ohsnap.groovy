9680c6a30093d91205c0f9ec9beb66a34bb03e29
Evgeni Golov <evgeni@redhat.com>
2018-02-22 11:34:56 +0100

    migrate ohsnap tests to a pipeline

---
+++ workflows/test/ohsnap.groovy
@@ -0,0 +1,64 @@
+def ruby = '2.4'
+
+pipeline {
+    agent { label 'rvm' }
+
+    stages {
+        stage('Setup Git Repos') {
+            steps {
+                gitlab_clone_and_merge('satellite6/ohsnap')
+            }
+        }
+
+        stage('Configure Environment') {
+            parallel {
+                stage('bundle') {
+                    steps {
+                        withRVM(['bundle install'], ruby)
+                    }
+                }
+                stage('npm') {
+                    steps {
+                        sh "npm install"
+                    }
+                }
+            }
+        }
+
+        stage('Run Tests') {
+            parallel {
+                stage('unittests') {
+                    steps {
+                        withRVM(['bundle exec rake'], ruby)
+                    }
+                }
+                stage('webpack') {
+                    steps {
+                        sh "./node_modules/webpack/bin/webpack.js --bail"
+                    }
+                }
+            }
+        }
+    }
+
+    post {
+        always {
+            archiveArtifacts artifacts: "Gemfile.lock, package-lock.json"
+            step([$class: 'CoberturaPublisher', coberturaReportFile: 'coverage/coverage.xml'])
+            cleanup(ruby)
+            deleteDir()
+        }
+    }
+}
+
+def cleanup(ruby = my_ruby) {
+    try {
+
+        sh "rm -rf node_modules/"
+
+    } finally {
+
+        cleanup_rvm(ruby)
+
+    }
+}
