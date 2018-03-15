827afed0fe82323d6a02809f8e4391541c36c770
Evgeni Golov <evgeni@golov.de>
2018-03-15 13:10:00 +0100

    create individual kerberos credential caches per run (#1055)

--- workflows/lib/kerberos.groovy
+++ workflows/lib/kerberos.groovy
@@ -1,12 +1,21 @@
 def kerberos_setup() {
+    def krbcc = sh(script: "mktemp ${pwd()}/.krbcc.XXXXXX", returnStdout: true).trim()
+
     withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'jenkins-satellite-jenkins.rhev-ci-vms.eng.rdu2.redhat.com.keytab', passwordVariable: 'KRB5_KEYTAB_BASE64', usernameVariable: 'KRB5_KEYTAB_PRINCIPAL']]) {
-        sh "echo ${KRB5_KEYTAB_BASE64} | base64 -d > JenkinsAccount.keytab"
-        sh "kinit -kt JenkinsAccount.keytab ${KRB5_KEYTAB_PRINCIPAL}"
-        sh "rm -f JenkinsAccount.keytab"
+        withEnv(["KRB5CCNAME=${krbcc}"]) {
+            sh "echo ${KRB5_KEYTAB_BASE64} | base64 -d > JenkinsAccount.keytab"
+            sh "kinit -kt JenkinsAccount.keytab ${KRB5_KEYTAB_PRINCIPAL}"
+            sh "rm -f JenkinsAccount.keytab"
+        }
     }
+
+    return krbcc
 }
 
-def kerberos_cleanup() {
-    sh "kdestroy"
+def kerberos_cleanup(krbcc) {
+    withEnv(["KRB5CCNAME=${krbcc}"]) {
+        sh "kdestroy"
+    }
     sh "rm -f JenkinsAccount.keytab"
+    sh "rm -f ${krbcc}"
 }
