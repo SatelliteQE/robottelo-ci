59572e1dafa0e9bf0638e025606d22f26407b6c7
sthirugn <sthirugn@redhat.com>
2018-03-23 11:17:51 -0400

    Added multiple platform support for mass registration job
    1. Support for multiple container platforms like RHEL7, RHEL72, etc.
    2. Support for bring your own container startup file

--- scripts/satellite6-mass-registration.sh
+++ scripts/satellite6-mass-registration.sh
@@ -6,9 +6,10 @@ echo "Setting up Container Host"
 if [ "${SETUP_CONTAINER_HOST}" == "true" ]; then
     cd playbooks
     # Note that the following playbook is temporarily hardcoded to generate rhel74 image.
-    ansible-playbook --inventory=${CONTAINER_HOST}, --extra-vars "RHN_USERNAME=${RHN_USERNAME} RHN_PASSWORD=${RHN_PASSWORD} RHN_POOLID=${RHN_POOLID} WORKSPACE=${WORKSPACE}" chd-setup.yaml
+    ansible-playbook --inventory=${CONTAINER_HOST}, --extra-vars "RHN_USERNAME=${RHN_USERNAME} RHN_PASSWORD=${RHN_PASSWORD} RHN_POOLID=${RHN_POOLID} WORKSPACE=${WORKSPACE} CUSTOM_CONTAINER_STARTUP_FILE=${CUSTOM_CONTAINER_STARTUP_FILE} CONTAINER_OS=${CONTAINER_OS}" chd-setup.yaml
 fi
 
 echo "Run the command for the container hosts registration"
+CONTAINER_TAG=$(echo "$CONTAINER_OS" | tr '[:upper:]' '[:lower:]')
 cd ${WORKSPACE}/playbooks
-ansible-playbook --inventory=${CONTAINER_HOST}, --extra-vars "SATELLITE_HOST=${SATELLITE_HOST} CONTENT_HOST_PREFIX=${CONTENT_HOST_PREFIX} ACTIVATION_KEY=${ACTIVATION_KEY} NUMBER_OF_HOSTS=${NUMBER_OF_HOSTS} EXIT_CRITERIA=${EXIT_CRITERIA}" chd-run.yaml
+ansible-playbook --inventory=${CONTAINER_HOST}, --extra-vars "SATELLITE_HOST=${SATELLITE_HOST} CONTENT_HOST_PREFIX=${CONTENT_HOST_PREFIX} ACTIVATION_KEY=${ACTIVATION_KEY} NUMBER_OF_HOSTS=${NUMBER_OF_HOSTS} EXIT_CRITERIA=${EXIT_CRITERIA} CONTAINER_TAG=${CONTAINER_TAG}" chd-run.yaml
