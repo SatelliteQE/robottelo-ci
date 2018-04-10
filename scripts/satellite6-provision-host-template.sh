a819953ea70f9107d36df90a0007727b0f290a11
Kedar Bidarkar <kbidarka@redhat.com>
2018-04-10 15:12:46 +0530

    Some minor fixes to get the package install successful
    Signed-off-by: Kedar Bidarkar <kbidarka@redhat.com>

--- scripts/satellite6-provision-host-template.sh
+++ scripts/satellite6-provision-host-template.sh
@@ -2,12 +2,10 @@
 RHEL7_LIBVIRT_HOST="rhel-7-libvirt"
 RHEL6_LIBVIRT_HOST="rhel-6-libvirt"
 
-set +e
-
 # Provision a host with Libvirt Provider.
-satellite_runner host create --name="${RHEL7_LIBVIRT_HOST}" --root-pass='changeme' --organization-id="${ORG}" --location-id="${LOC}" --hostgroup="RHEL 7 Server 64-bit HG" --compute-resource="$COMPUTE_RESOURCE_NAME_LIBVIRT" --compute-attributes="cpus=1, memory=1073741824, start=1" --interface="primary=true, compute_type=bridge, compute_bridge=${SUBNET_NAME}, compute_model=virtio" --volume="capacity=10G,format_type=qcow2"
+satellite_runner host create --name="${RHEL7_LIBVIRT_HOST}" --root-password='changeme' --organization-id="${ORG}" --location-id="${LOC}" --hostgroup="RHEL 7 Server 64-bit HG" --compute-resource="$COMPUTE_RESOURCE_NAME_LIBVIRT" --compute-attributes="cpus=1, memory=1073741824, start=1" --interface="primary=true, compute_type=bridge, compute_bridge=${SUBNET_NAME}, compute_model=virtio" --volume="capacity=10G,format_type=qcow2"
 
-satellite_runner host create --name="${RHEL6_LIBVIRT_HOST}" --root-pass='changeme' --organization-id="${ORG}" --location-id="${LOC}" --hostgroup="RHEL 6 Server 64-bit HG" --compute-resource="$COMPUTE_RESOURCE_NAME_LIBVIRT" --compute-attributes="cpus=1, memory=1073741824, start=1" --interface="primary=true, compute_type=bridge, compute_bridge=${SUBNET_NAME}, compute_model=virtio" --volume="capacity=10G,format_type=qcow2"
+satellite_runner host create --name="${RHEL6_LIBVIRT_HOST}" --root-password='changeme' --organization-id="${ORG}" --location-id="${LOC}" --hostgroup="RHEL 6 Server 64-bit HG" --compute-resource="$COMPUTE_RESOURCE_NAME_LIBVIRT" --compute-attributes="cpus=1, memory=1073741824, start=1" --interface="primary=true, compute_type=bridge, compute_bridge=${SUBNET_NAME}, compute_model=virtio" --volume="capacity=10G,format_type=qcow2"
 
 DOMAIN_NAME=$(satellite --csv domain list  | awk -F "," '!/Name/ {print $2}')
 
@@ -18,7 +16,7 @@ echo "RHEL7 hosts IP is: ${RHEL7_IP}"
 echo "RHEL6 hosts IP is: ${RHEL6_IP}"
 
 # This sleep is needed, because of the provisioning phase during this period.
-sleep 1200
+sleep 1800
 
 satellite_runner host package install --host "${RHEL7_LIBVIRT_HOST}.${DOMAIN_NAME}"  --packages zsh
 
@@ -27,5 +25,3 @@ satellite_runner host package install --host "${RHEL6_LIBVIRT_HOST}.${DOMAIN_NAM
 satellite_runner host delete --name "${RHEL7_LIBVIRT_HOST}.${DOMAIN_NAME}"
 
 satellite_runner host delete --name "${RHEL6_LIBVIRT_HOST}.${DOMAIN_NAME}"
-
-set -e
