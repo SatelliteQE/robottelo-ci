23333d68bf7323865ee51044d8e5dd983ab8a91c
Lukas Pramuk <lpramuk@redhat.com>
2018-04-18 18:45:00 +0200

    Split tiers into multiple instances also for upstream (otherwise higher tier results are too bad)

--- scripts/satellite6-graceful-shutdown.sh
+++ scripts/satellite6-graceful-shutdown.sh
@@ -1,13 +1,13 @@
-if ! [[ ${SATELLITE_DISTRIBUTION} =~ UPSTREAM|KOJI ]]; then
-    echo "Shutting down the Base instance of ${SERVER_HOSTNAME} gracefully"
-    # Shutdown the Satellite6 services before shutdown.
-    ssh -o StrictHostKeyChecking=no root@"${SERVER_HOSTNAME}" katello-service stop
-    # Try to shutdown the Satellite6 instance gracefully and sleep for a while.
-    ssh -o StrictHostKeyChecking=no root@"${PROVISIONING_HOST}" virsh shutdown ${TARGET_IMAGE}
-    sleep 120
-    set +e
-    # Destroy the sat6 instance anyways if for some reason virsh shutdown couldn't gracefully shut it down.
-    ssh -o StrictHostKeyChecking=no root@"${PROVISIONING_HOST}" virsh destroy ${TARGET_IMAGE}
-    set -e
-    echo "========================================"
-fi
+ssh_opts='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+
+echo "Shutting down the Base instance of ${SERVER_HOSTNAME} gracefully"
+# Shutdown the Satellite6 services before shutdown.
+ssh $ssh_opts root@"${SERVER_HOSTNAME}" katello-service stop
+# Try to shutdown the Satellite6 instance gracefully and sleep for a while.
+ssh $ssh_opts root@"${PROVISIONING_HOST}" virsh shutdown ${TARGET_IMAGE}
+sleep 120
+set +e
+# Destroy the sat6 instance anyways if for some reason virsh shutdown couldn't gracefully shut it down.
+ssh $ssh_opts root@"${PROVISIONING_HOST}" virsh destroy ${TARGET_IMAGE}
+set -e
+echo "========================================"
