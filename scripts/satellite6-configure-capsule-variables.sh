67e1cfdc66c77eb17ec28cb1a3cf5a0ba9850bb6
Kedar Bidarkar <kbidarka@redhat.com>
2018-04-18 20:35:08 +0530

    Adding the capsule-installer job
    Signed-off-by: Kedar Bidarkar <kbidarka@redhat.com>

--- scripts/satellite6-configure-capsule-variables.sh
+++ scripts/satellite6-configure-capsule-variables.sh
@@ -6,7 +6,7 @@ echo
 
 
 # Populates the HTTP Server information.
-sed -i "s|ADMIN_PASSWORD=.*|ADMIN_PASSWORD=\"${ADMIN_PASSWORD}\"|" satellite6-populate-capsule.sh
+sed -i "s|^ADMIN_PASSWORD=.*|ADMIN_PASSWORD=\"${ADMIN_PASSWORD}\"|" satellite6-populate-capsule.sh
 
 # TODO Populates the DOWNLOAD_POLICY setting for Capsule. Future support.
 # sed -i "s|DOWNLOAD_POLICY=.*|DOWNLOAD_POLICY=\"${DOWNLOAD_POLICY}\"|" satellite6-populate-capsule.sh
@@ -19,9 +19,9 @@ if [ -z "${BRIDGE}" ]; then
     export BRIDGE="foreman"
 fi
 sed -i "s|SUBNET_NAME=.*|SUBNET_NAME=\"${BRIDGE}\"|" satellite6-populate-capsule.sh
-sed -i "s|SUBNET_RANGE=.*|SUBNET_RANGE=\"${SUBNET_RANGE}\"|" satellite6-populate-capsule.sh
-sed -i "s|SUBNET_MASK=.*|SUBNET_MASK=\"${SUBNET_MASK}\"|" satellite6-populate-capsule.sh
-sed -i "s|SUBNET_GATEWAY=.*|SUBNET_GATEWAY=\"${SUBNET_GATEWAY}\"|" satellite6-populate-capsule.sh
+sed -i "s|SUBNET_RANGE=.*|SUBNET_RANGE=\"${SUBNET}\"|" satellite6-populate-capsule.sh
+sed -i "s|SUBNET_MASK=.*|SUBNET_MASK=\"${NETMASK}\"|" satellite6-populate-capsule.sh
+sed -i "s|SUBNET_GATEWAY=.*|SUBNET_GATEWAY=\"${GATEWAY}\"|" satellite6-populate-capsule.sh
 
 # Set Satellite6 Version, So that MEDIUM-ID is used accordingly. Note, Sat6.3 may not use MEDIUM.
 sed -i "s|SAT_VERSION=.*|SAT_VERSION=\"${SATELLITE_VERSION}\"|" satellite6-populate-capsule.sh
