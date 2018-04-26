6fca49d1bcd1735617af5fa99622da379a3f4049
Evgeni Golov <evgeni@golov.de>
2018-04-26 07:41:15 +0200

    add ansible release pipe (#1131)

---
+++ workflows/ansiblePipeline.groovy
@@ -0,0 +1,35 @@
+node('rhel') {
+
+    stage("Setup Workspace") {
+
+        deleteDir()
+        setupAnsibleEnvironment {}
+
+    }
+
+    stage("Sync Repositories") {
+        def products = [
+            'Red Hat Ansible Engine',
+        ]
+
+        runPlaybookInParallel {
+            name = "sync"
+            items = products
+            item_name = 'product'
+            playbook = 'playbooks/sync_products.yml'
+        }
+    }
+
+    stage("Publish Content Views") {
+        def content_views = [
+            'Ansible',
+        ]
+
+        runPlaybookInParallel {
+            name = "publish"
+            items = content_views
+            item_name = 'content_view'
+            playbook = 'playbooks/publish_content_views.yml'
+        }
+    }
+}
