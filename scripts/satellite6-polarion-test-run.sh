986b12f0a90fc0532af27aa140e6d292bea13acc
Kedar Bidarkar <kbidarka@redhat.com>
2018-05-15 21:25:19 +0530

    Run polarion-test-run job immediately after the tier job completed
    Signed-off-by: Kedar Bidarkar <kbidarka@redhat.com>

---
+++ scripts/satellite6-polarion-test-run.sh
@@ -0,0 +1,57 @@
+# Populate token-prefix and betelgeuse depending upon Satellite6 Version.
+if [[ "${SATELLITE_VERSION}" = "6.1" ]] || [[ "${SATELLITE_VERSION}" = "6.2" ]] ; then
+    pip install "betelgeuse<0.8"    
+    TOKEN_PREFIX="--token-prefix=@" 
+else
+    pip install betelgeuse
+    TOKEN_PREFIX=""
+fi
+  
+# Create a new release with POLARION_RELEASE as the parent-plan.
+
+if [ -n "$POLARION_RELEASE" ]; then
+    betelgeuse test-plan --name "${POLARION_RELEASE}" --plan-type release \
+    "${POLARION_PROJECT}"
+else
+    echo "Please specify the POLARION_RELEASE"
+    exit 1
+fi
+  
+# Create a new iteration for the current run
+if [ -n "$POLARION_RELEASE" ]; then
+    betelgeuse test-plan --name "${TEST_RUN_ID}" --parent-name "${POLARION_RELEASE}" \
+        --plan-type iteration "${POLARION_PROJECT}" 
+else
+    betelgeuse test-plan --name "${TEST_RUN_ID}" \
+        --plan-type iteration "${POLARION_PROJECT}" 
+fi
+  
+POLARION_SELECTOR="name=Satellite 6"
+SANITIZED_ITERATION_ID="$(echo ${TEST_RUN_ID} | sed 's|\.|_|g' | sed 's| |_|g')"
+  
+# Prepare the XML files
+
+for run in parallel sequential; do
+    betelgeuse ${TOKEN_PREFIX} xml-test-run \
+    --custom-fields "isautomated=true" \
+    --custom-fields "arch=x8664" \
+    --custom-fields "variant=server" \
+    --custom-fields "plannedin=${SANITIZED_ITERATION_ID}" \
+    --response-property "${POLARION_SELECTOR}" \
+    --test-run-id "${TEST_RUN_ID} - ${run} - Tier ${ENDPOINT##tier}" \
+    "./tier${ENDPOINT##tier}-${run}-results.xml" \
+    tests/foreman \
+    "${POLARION_USERNAME}" \
+    "${POLARION_PROJECT}" \
+    "polarion-tier${ENDPOINT##tier}-${run}-results.xml"
+curl -k -u "${POLARION_USERNAME}:${POLARION_PASSWORD}" \
+    -X POST \
+    -F file=@polarion-tier${ENDPOINT##tier}-${run}-results.xml \
+    "${POLARION_URL}import/xunit"
+done
+
+# Mark the iteration done
+betelgeuse test-plan \
+    --name "${TEST_RUN_ID}" \
+    --custom-fields status=done \
+    "${POLARION_PROJECT}"
