4419bc563e18d8e62f7c84df6b9c2936ec3aea35
Kedar Bidarkar <kbidarka@redhat.com>
2018-05-22 00:15:10 +0530

    Added support to upload test run results of rhai and destructive jobs to polarion
    Signed-off-by: Kedar Bidarkar <kbidarka@redhat.com>

--- scripts/satellite6-betelgeuse-test-run.sh
+++ scripts/satellite6-betelgeuse-test-run.sh
@@ -68,24 +68,41 @@ if [[ "${TEST_RUN_ID}" = *"upgrade"* ]]; then
     -X POST \
     -F file=@polarion-smoke-upgrade-results.xml \
     "${POLARION_URL}import/xunit"
-else
-    for run in parallel sequential; do
-        betelgeuse ${TOKEN_PREFIX} xml-test-run \
+elif [ "${ENDPOINT}" = "rhai" ] || [ "${ENDPOINT}" = "destructive" ]; then
+    betelgeuse ${TOKEN_PREFIX} xml-test-run \
         --custom-fields "isautomated=true" \
         --custom-fields "arch=x8664" \
         --custom-fields "variant=server" \
         --custom-fields "plannedin=${SANITIZED_ITERATION_ID}" \
         --response-property "${POLARION_SELECTOR}" \
-        --test-run-id "${TEST_RUN_ID} - ${run} - Tier ${ENDPOINT##tier}" \
-        "./tier${ENDPOINT##tier}-${run}-results.xml" \
+        --test-run-id "${TEST_RUN_ID} - ${ENDPOINT##tier}" \
+        "./foreman-results.xml" \
         tests/foreman \
         "${POLARION_USERNAME}" \
         "${POLARION_PROJECT}" \
-        "polarion-tier${ENDPOINT##tier}-${run}-results.xml"
+        "polarion-${ENDPOINT##tier}-foreman-results.xml"
     curl -k -u "${POLARION_USERNAME}:${POLARION_PASSWORD}" \
         -X POST \
-        -F file=@polarion-tier${ENDPOINT##tier}-${run}-results.xml \
+        -F file=@polarion-${ENDPOINT##tier}-foreman-results.xml \
         "${POLARION_URL}import/xunit"
+else
+    for run in parallel sequential; do
+        betelgeuse ${TOKEN_PREFIX} xml-test-run \
+            --custom-fields "isautomated=true" \
+            --custom-fields "arch=x8664" \
+            --custom-fields "variant=server" \
+            --custom-fields "plannedin=${SANITIZED_ITERATION_ID}" \
+            --response-property "${POLARION_SELECTOR}" \
+            --test-run-id "${TEST_RUN_ID} - ${run} - Tier ${ENDPOINT##tier}" \
+            "./tier${ENDPOINT##tier}-${run}-results.xml" \
+            tests/foreman \
+            "${POLARION_USERNAME}" \
+            "${POLARION_PROJECT}" \
+            "polarion-tier${ENDPOINT##tier}-${run}-results.xml"
+        curl -k -u "${POLARION_USERNAME}:${POLARION_PASSWORD}" \
+            -X POST \
+            -F file=@polarion-tier${ENDPOINT##tier}-${run}-results.xml \
+            "${POLARION_URL}import/xunit"
     done
 fi
 
