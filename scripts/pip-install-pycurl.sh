8a685e1890929f113f5633eb5043e62339263098
ntkathole <nikhilkathole2683@gmail.com>
2018-06-04 13:12:05 +0530

    Fix pycurl config error

--- scripts/pip-install-pycurl.sh
+++ scripts/pip-install-pycurl.sh
@@ -2,7 +2,9 @@ pip install -U pip
 pip install six
 
 if [ "$(curl --version | grep NSS 2>/dev/null)" ]; then
+    export PYCURL_SSL_LIBRARY=nss
     pip install --compile --install-option="--with-nss" pycurl
 else
+    export PYCURL_SSL_LIBRARY=openssl
     pip install --compile --install-option="--with-openssl" pycurl
 fi
