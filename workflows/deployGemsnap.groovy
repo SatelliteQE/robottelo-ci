b6f2b584f4caafb5f4e60f20dd75551ad0f69f4f
Zjhuntin <zhunting@redhat.com>
2018-07-19 09:23:38 -0400

    Rebuild both gemsnaps on merge

--- workflows/deployGemsnap.groovy
+++ workflows/deployGemsnap.groovy
@@ -1,7 +1,8 @@
 node('sat6-rhel7') {
     stage("Push to Open Platform") {
         withCredentials([string(credentialsId: 'GEMSNAP_OPEN_PAAS_WEBHOOK_URL', variable: 'GEMSNAP_OPEN_PAAS_WEBHOOK_URL')]) {
-            sh "curl -k -XPOST ${GEMSNAP_OPEN_PAAS_WEBHOOK_URL}"
+            sh "curl -k -XPOST ${GEMSNAP_63_OPEN_PAAS_WEBHOOK_URL}"
+            sh "curl -k -XPOST ${GEMSNAP_64_OPEN_PAAS_WEBHOOK_URL}"
 	}
     }
 }
