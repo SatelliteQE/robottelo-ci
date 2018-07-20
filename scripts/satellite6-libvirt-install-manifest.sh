89ae684b68f3e7b94159b85faaac80490d6e0fe1
Kedar Bidarkar <kbidarka@redhat.com>
2018-07-20 18:52:25 +0530

    update the manifest to robotello-automation
    Signed-off-by: Kedar Bidarkar <kbidarka@redhat.com>

--- scripts/satellite6-libvirt-install-manifest.sh
+++ scripts/satellite6-libvirt-install-manifest.sh
@@ -1,3 +1,3 @@
 if [[ -z "${MANIFEST_LOCATION_URL}" ]]; then
-    MANIFEST_LOCATION_URL="${HTTP_SERVER_HOSTNAME}/manifests/manifest_kbidarka.zip"
-fi 
+    MANIFEST_LOCATION_URL="${HTTP_SERVER_HOSTNAME}/manifests/manifest-latest.zip"
+fi
