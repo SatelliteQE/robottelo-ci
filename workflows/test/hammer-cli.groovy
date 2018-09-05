ec7ecb15597be8ffc1b65429a2191b55864481b4
Evgeni Golov <evgeni@golov.de>
2018-09-05 16:24:06 +0200

    test hammer_cli_katello with a pipeline (#1278)

--- workflows/test/hammer-cli.groovy
+++ workflows/test/hammer-cli.groovy
@@ -1,3 +1,6 @@
+def version_map = branch_map[env.gitlabTargetBranch]
+def ruby = version_map['ruby']
+
 node('sat6-build') {
 
     stage('Setup Git Repos') {
@@ -9,18 +12,24 @@ node('sat6-build') {
 
     stage('Run Tests') {
 
+        if (plugin_name == 'hammer_cli_katello') {
+            test_command = 'rake'
+        } else {
+            test_command = 'rake ci:setup:minitest test'
+        }
+
         try {
 
             gitlabCommitStatus {
-                withRVM(['bundle install'], 2.2)
-                withRVM(['bundle exec rake ci:setup:minitest test TESTOPTS="-v"'], 2.2)
+                withRVM(['bundle install'], ruby)
+                withRVM(["bundle exec ${test_command} TESTOPTS='-v'"], ruby)
             }
 
         } finally {
 
             archive "Gemfile.lock"
 
-            cleanup()
+            cleanup_rvm('', ruby)
 
         }
     }
