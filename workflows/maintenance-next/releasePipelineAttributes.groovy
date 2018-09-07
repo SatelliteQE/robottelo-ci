fb01eab04f17ed3a16ae563da4bccd41e0c599bc
Evgeni Golov <evgeni@redhat.com>
2018-09-07 11:04:31 +0200

    fix release-qa for sat-maint(-next)

--- workflows/maintenance-next/releasePipelineAttributes.groovy
+++ workflows/maintenance-next/releasePipelineAttributes.groovy
@@ -1,5 +1,5 @@
 def satellite_version = 'satellite-maintenance-next'
 
-def content_views = 'Satellite Maintenance Next RHEL7'
+def release_content_view = 'Satellite Maintenance Next RHEL7'
 
-def composite_content_view = 'Satellite Maintenance Next with RHEL7 Server'
+def release_composite_content_view = 'Satellite Maintenance Next with RHEL7 Server'
