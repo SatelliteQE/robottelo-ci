0aa6f15553bb5432ead28f2060056d4c0eed8ae7
Chris Roberts <chrobert@redhat.com>
2018-09-27 12:51:49 -0400

    Add support for VMware Compute Resource prov.

--- scripts/satellite6-provision-host-template.sh
+++ scripts/satellite6-provision-host-template.sh
@@ -1,6 +1,7 @@
 
 RHEL7_LIBVIRT_HOST="rhel-7-libvirt"
 RHEL6_LIBVIRT_HOST="rhel-6-libvirt"
+VMWARE_HOST="rhel-7-vmware"
 
 # Provision a host with Libvirt Provider.
 satellite_runner host create --name="${RHEL7_LIBVIRT_HOST}" --root-password='changeme' --organization-id="${ORG}" --location-id="${LOC}" --hostgroup="RHEL 7 Server 64-bit HG" --compute-resource="$COMPUTE_RESOURCE_NAME_LIBVIRT" --compute-attributes="cpus=1, memory=1073741824, start=1" --interface="primary=true, compute_type=bridge, compute_bridge=${SUBNET_NAME}, compute_model=virtio" --volume="capacity=10G,format_type=qcow2"
@@ -25,3 +26,11 @@ sleep 1200
 satellite_runner host delete --name "${RHEL7_LIBVIRT_HOST}.${DOMAIN_NAME}"
 
 satellite_runner host delete --name "${RHEL6_LIBVIRT_HOST}.${DOMAIN_NAME}"
+
+# Provision a host with VMware Provider.
+satellite_runner host create --name="${VMWARE_HOST}" --root-password='changeme' --domain-id "${DOMAIN_ID}" --subnet "${SUBNET_NAME}" --organization-id="${ORG}" --build true --location-id="${LOC}" --hostgroup="RHEL 7 Server 64-bit HG" --provision-method build --compute-resource="$COMPUTE_RESOURCE_NAME_VMWARE" --interface="managed=true,primary=true,provision=true,compute_type=VirtualE1000,compute_network=\"qe_${SUBNET_NAME}\"" --volume="size_gb=20G,datastore=Local-Ironforge,name=myharddisk,thin=true,eager_zero=false,mode=persistent" --compute-attributes="cpus=1,corespersocket=2,memory_mb=4096,cluster=Satellite_Engineering,path=/Datacenters/RH_Engineering/vm,start=1"
+
+# This sleep is needed, because of the provisioning phase during this period.
+sleep 1200
+
+satellite_runner host delete --name "${VMWARE_HOST}.${DOMAIN_NAME}"
