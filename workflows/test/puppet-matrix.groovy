190cfa77157cacf92ed2b636ade70e2828588555
Zachary Huntington-Meath <zhunting@redhat.com>
2018-10-05 03:03:11 -0400

    Add basic Satellite 6.5 jobs (#1309)

--- workflows/test/puppet-matrix.groovy
+++ workflows/test/puppet-matrix.groovy
@@ -2,5 +2,6 @@ def puppet_ruby = [
     [puppet_version: '3.8', ruby_version: '1.8.7', satellite_versions: ['6.2.0']],
     [puppet_version: '3.8', ruby_version: '2.0.0', satellite_versions: ['6.2.0', '6.3.0']],
     [puppet_version: '4.10.9', ruby_version: '2.1.9', satellite_versions: ['6.3.0']],
-    [puppet_version: '5.2.0', ruby_version: '2.4.3', satellite_versions: ['6.4.0']]
+    [puppet_version: '5.2.0', ruby_version: '2.4.3', satellite_versions: ['6.4.0']],
+    [puppet_version: '5.2.0', ruby_version: '2.4.3', satellite_versions: ['6.5.0']]
 ]
