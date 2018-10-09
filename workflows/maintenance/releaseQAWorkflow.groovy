fc83ffcbe5b23f9e8f23ffbbf7b04ee1c84c6155
Evgeni Golov <evgeni@golov.de>
2018-10-09 15:45:53 +0200

    enable archiving for sat-maint(-next) (#1316)

--- workflows/maintenance/releaseQAWorkflow.groovy
+++ workflows/maintenance/releaseQAWorkflow.groovy
@@ -29,4 +29,33 @@ node('sat6-build') {
         }
 
     }
+
+    stage("Create Archive Environment") {
+
+      // Work around for parameters not being accessible in functions
+      writeFile file: 'snap_version', text: snapVersion
+      def version = readFile 'snap_version'
+
+      createLifecycleEnvironment {
+          name = version
+          prior = 'Library'
+          organization = 'Sat6-CI'
+      }
+
+    }
+
+    stage("Archive Satellite Maintenance") {
+
+      // Work around for parameters not being accessible in functions
+      writeFile file: 'snap_version', text: snapVersion
+      def version = readFile 'snap_version'
+
+      promoteContentView {
+        organization = 'Sat6-CI'
+        content_view = release_composite_content_view
+        from_lifecycle_environment = 'QA'
+        to_lifecycle_environment = version
+      }
+
+    }
 }
