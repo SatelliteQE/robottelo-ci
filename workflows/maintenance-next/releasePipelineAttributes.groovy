65a0753aa6d840f804ddec2f2b017c05f6fcc619
Evgeni Golov <evgeni@redhat.com>
2018-10-11 11:33:18 +0200

    unify sat-maint(-next) release-pipeline

--- workflows/maintenance-next/releasePipelineAttributes.groovy
+++ workflows/maintenance-next/releasePipelineAttributes.groovy
@@ -1,4 +1,16 @@
-def satellite_version = 'satellite-maintenance-next'
+def compose_versions = ['7']
+def compose_tag = 'next'
+def os_versions = ['7']
+def satellite_version = 'maintenance-next'
+def products = [
+    'Satellite Maintenance Next Composes'
+]
+def content_views = [
+    'Satellite Maintenance Next RHEL7'
+]
+def composite_content_views = [
+    'Satellite Maintenance Next with RHEL7 Server'
+]
 
 def release_content_view = 'Satellite Maintenance Next RHEL7'
 
