d1b5c9e43ffe8b4fff3710d047bdf84bc3e0c65a
Evgeni Golov <evgeni@golov.de>
2018-10-12 14:48:01 +0200

    move forklift testing to an own function (#1326)

--- workflows/lib/libvirt.groovy
+++ workflows/lib/libvirt.groovy
@@ -3,3 +3,32 @@ def runOnLibvirtHost(action) {
         sh "ssh jenkins@${env.LIBVIRT_HOST} \"${action}\""
     }
 }
+
+def test_forklift(args) {
+
+    def os_versions = args.os_versions ?: ['7']
+    def sattellite_version = args.sattellite_version
+
+    runOnLibvirtHost "cd sat-deploy && git -c http.sslVerify=false fetch origin && git reset origin/master --hard"
+    runOnLibvirtHost "cd sat-deploy/forklift && git -c http.sslVerify=false fetch origin && git reset origin/master --hard"
+
+    def branches = [:]
+
+    wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "xterm"]) {
+        for (int i = 0; i < os_versions.size(); i++) {
+            def index = i // fresh variable per iteration; i will be mutated
+            def item = os_versions.get(index)
+
+            branches["install-rhel-${item}"] = {
+                try {
+                    runOnLibvirtHost "cd sat-deploy && ansible-playbook pipelines/compose_test_${satellite_version}_rhel${item}.yml -e forklift_state=up"
+                } finally {
+                    runOnLibvirtHost "cd sat-deploy && ansible-playbook pipelines/compose_test_${satellite_version}_rhel${item}.yml -e forklift_state=destroy"
+                }
+            }
+        }
+    }
+
+    parallel branches
+
+}
