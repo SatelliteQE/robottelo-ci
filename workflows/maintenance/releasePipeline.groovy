eefd06a65ab2c1742c79126a86e1a30f6a562ead
Evgeni Golov <evgeni@redhat.com>
2018-10-12 10:20:38 +0200

    move forklift testing to an own function

--- workflows/maintenance/releasePipeline.groovy
+++ workflows/maintenance/releasePipeline.groovy
@@ -35,27 +35,10 @@ node('sat6-build') {
 
     stage("Test Installation") {
 
-        runOnLibvirtHost "cd sat-deploy && git -c http.sslVerify=false fetch origin && git reset origin/master --hard"
-        runOnLibvirtHost "cd sat-deploy/forklift && git -c http.sslVerify=false fetch origin && git reset origin/master --hard"
-
-        def branches = [:]
-
-        wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "xterm"]) {
-            for (int i = 0; i < os_versions.size(); i++) {
-                def index = i // fresh variable per iteration; i will be mutated
-                def item = os_versions.get(index)
-
-                branches["install-rhel-${item}"] = {
-                    try {
-                        runOnLibvirtHost "cd sat-deploy && ansible-playbook pipelines/compose_test_${satellite_version}_rhel${item}.yml -e forklift_state=up"
-                    } finally {
-                        runOnLibvirtHost "cd sat-deploy && ansible-playbook pipelines/compose_test_${satellite_version}_rhel${item}.yml -e forklift_state=destroy"
-                    }
-                }
-            }
-        }
-
-        parallel branches
+        test_forklift(
+            os_versions: os_versions,
+            satellite_version: satellite_version
+        )
 
     }
 
