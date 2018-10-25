1775a03f4b814bd9c4a61d95ab804b186e3d89ff
Evgeni Golov <evgeni@redhat.com>
2018-10-25 09:56:48 +0200

    migrate theme-satellite to a new-style pipeline
    and enable testing asset compilation

--- workflows/test/foreman-theme-satellite.groovy
+++ workflows/test/foreman-theme-satellite.groovy
@@ -3,69 +3,118 @@ def plugin_name = 'foreman_theme_satellite'
 def version_map = branch_map[env.gitlabTargetBranch]
 def ruby = version_map['ruby']
 
-node('sat6-build') {
-
-    stage('Setup Git Repos') {
-
-        deleteDir()
-
-        dir('foreman') {
-            if (env.gitlabTargetBranch == 'master' ) {
-                git url: "https://github.com/theforeman/foreman.git", branch: "develop"
-            } else {
-                gitlab_clone('foreman')
-            }
-        }
-
-        dir('plugin') {
-            gitlab_clone_and_merge(plugin_name)
-        }
-
+pipeline {
+    options {
+        timestamps()
+        timeout(time: 2, unit: 'HOURS')
+        ansiColor('xterm')
+        buildDiscarder(logRotator(numToKeepStr: '15'))
     }
 
-    stage('Configure Environment') {
+    agent { label 'sat6-build' }
 
-        dir('foreman') {
-            configure_foreman_environment()
-        }
+    stages {
 
-    }
+        stage('Setup Git Repos') {
 
-    stage('Configure Database') {
+            steps {
 
-        dir('foreman') {
-            setup_foreman(ruby)
-        }
+                deleteDir()
 
-    }
-
-    stage('Setup plugin') {
-
-        dir('foreman') {
-            setup_plugin(plugin_name, ruby)
-        }
-
-    }
-
-    stage('Run Tests') {
-
-        dir('foreman') {
-            try {
-
-                gitlabCommitStatus {
-                    withRVM(['bundle exec rake test:foreman_theme_satellite'], ruby)
-                    withRVM(['bundle exec rake db:drop db:create db:migrate'], ruby)
-                    withRVM(['bundle exec rake db:seed'], ruby)
+                dir('foreman') {
+                    script {
+                        if (env.gitlabTargetBranch == 'master' ) {
+                            git url: "https://github.com/theforeman/foreman.git", branch: "develop"
+                        } else {
+                            gitlab_clone('foreman')
+                        }
+                    }
                 }
 
-            } finally {
+                dir('plugin') {
+                    gitlab_clone_and_merge(plugin_name)
+                }
 
-                archive "Gemfile.lock pkg/*"
+            }
 
-                cleanup(ruby)
+        }
 
+        stage('Configure Environment') {
+
+            steps {
+
+                dir('foreman') {
+                    configure_foreman_environment()
+                }
+
+            }
+
+        }
+
+        stage('Configure Database') {
+
+            steps {
+
+                dir('foreman') {
+                    setup_foreman(ruby)
+                }
+
+            }
+
+        }
+
+        stage('Setup plugin') {
+
+            steps {
+
+                dir('foreman') {
+                    setup_plugin(plugin_name, ruby)
+                }
+
+            }
+
+        }
+
+        stage('Run Tests') {
+            parallel {
+                stage('tests') {
+                    steps {
+                        dir('foreman') {
+                            gitlabCommitStatus(name: "tests") {
+                                withRVM(['bundle exec rake test:foreman_theme_satellite'], ruby)
+                                withRVM(['bundle exec rake db:drop db:create db:migrate'], ruby)
+                                withRVM(['bundle exec rake db:seed'], ruby)
+                            }
+                        }
+                    }
+                }
+                stage('assets-precompile') {
+                    steps {
+                        dir('foreman') {
+                            gitlabCommitStatus(name: "assets-precompile") {
+                                sh "npm install npm"
+                                withRVM(["bundle exec node_modules/.bin/npm install"], ruby)
+                                withRVM(["bundle exec rake plugin:assets:precompile[${plugin_name}] RAILS_ENV=production --trace"], ruby)
+                            }
+                        }
+                    }
+                }
             }
         }
     }
 
+    post {
+        failure {
+            updateGitlabCommitStatus name: 'jenkins', state: 'failed'
+        }
+        success {
+            updateGitlabCommitStatus name: 'jenkins', state: 'success'
+        }
+        always {
+            dir('foreman') {
+                archiveArtifacts artifacts: "Gemfile.lock, pkg/*"
+                cleanup(ruby)
+            }
+        }
+    }
 }
