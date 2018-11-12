12fee7f8b17dcc9abec75478c9594f41305f1544
Lukas Pramuk <lpramuk@redhat.com>
2018-11-12 17:21:05 +0100

    New job: satellite6-baseimage-deploy - automatically creates and deploys baseimage

---
+++ scripts/satellite6-baseimage-deploy.sh
@@ -0,0 +1,12 @@
+pip install -r requirements.txt
+source ${CONFIG_FILES}
+source config/provisioning_environment.conf
+HYPERVISORS=${HYPERVISORS:-$PROVISIONING_HOSTS}
+set $HYPERVISORS # we can use 1st hypervisors as $1
+
+eval $(ssh-agent -s) # setup ssh agent
+ssh-add
+
+fab -A -D -H "root@$1" deploy_baseimage_by_url:$OS_URL,hypervisors=$HYPERVISORS
+
+eval $(ssh-agent -s -k) # teardown ssh agent
