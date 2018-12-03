f144bef424fd976e914bd2216c4af22a3aac961a
Perry Gagne <pgagne@redhat.com>
2018-12-03 11:50:39 -0500

    Fixed for  RHEL 6.10

--- scripts/satellite6-populate-template.sh
+++ scripts/satellite6-populate-template.sh
@@ -201,7 +201,7 @@ satellite_runner subscription upload --organization-id "${ORG}" --file "${HOME}"
 # Enable Red Hat repositories
 # Kickstart trees
 satellite_runner repository-set enable --name="Red Hat Enterprise Linux 7 Server (Kickstart)" --basearch="x86_64" --releasever="7.6" --product "Red Hat Enterprise Linux Server" --organization-id="${ORG}"
-satellite_runner repository-set enable --name="Red Hat Enterprise Linux 6 Server (Kickstart)" --basearch="x86_64" --releasever="6.9" --product "Red Hat Enterprise Linux Server" --organization-id="${ORG}"
+satellite_runner repository-set enable --name="Red Hat Enterprise Linux 6 Server (Kickstart)" --basearch="x86_64" --releasever="6.10" --product "Red Hat Enterprise Linux Server" --organization-id="${ORG}"
 
 # 'Base' OS RPMs
 satellite_runner repository-set enable --name="Red Hat Enterprise Linux 7 Server (RPMs)" --basearch="x86_64" --releasever="7Server" --product "Red Hat Enterprise Linux Server" --organization-id="${ORG}"
@@ -261,7 +261,7 @@ satellite_runner  content-view publish --name='RHEL 7 CV' --organization-id="${O
 satellite_runner  content-view version promote --content-view='RHEL 7 CV' --organization-id="${ORG}" --to-lifecycle-environment=DEV --from-lifecycle-environment="Library"
 
 # RHEL 6
-satellite_runner  content-view add-repository --name='RHEL 6 CV' --organization-id="${ORG}" --product='Red Hat Enterprise Linux Server' --repository='Red Hat Enterprise Linux 6 Server Kickstart x86_64 6.9'
+satellite_runner  content-view add-repository --name='RHEL 6 CV' --organization-id="${ORG}" --product='Red Hat Enterprise Linux Server' --repository='Red Hat Enterprise Linux 6 Server Kickstart x86_64 6.10'
 satellite_runner  content-view add-repository --name='RHEL 6 CV' --organization-id="${ORG}" --product='Red Hat Enterprise Linux Server' --repository='Red Hat Enterprise Linux 6 Server RPMs x86_64 6Server'
 satellite_runner  content-view add-repository --name='RHEL 6 CV' --organization-id="${ORG}" --product="${RHEL6_TOOLS_PRD}" --repository="${RHEL6_TOOLS_REPO}"
 satellite_runner  content-view publish --name='RHEL 6 CV' --organization-id="${ORG}"
@@ -434,7 +434,7 @@ satellite_runner capsule import-classes --id 1 --environment-id 1
 RHEL7_OS_ID=$(satellite --csv os list | grep "7.6" | cut -d ',' -f1 | grep -vi "^id")
 echo "RHEL7 OS ID is: ${RHEL7_OS_ID}"
 
-RHEL6_OS_ID=$(satellite --csv os list | grep "6.9" | cut -d ',' -f1 | grep -vi "^id")
+RHEL6_OS_ID=$(satellite --csv os list | grep "6.10" | cut -d ',' -f1 | grep -vi "^id")
 echo "RHEL6 OS ID is: ${RHEL6_OS_ID}"
 
 if [ "${SAT_VERSION}" = "6.3" ]; then
