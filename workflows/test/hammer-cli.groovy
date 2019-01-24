a18b5ef1c0ec73472bfc5dc028eb2dc8d9218c96
Evgeni Golov <evgeni@redhat.com>
2019-01-24 14:42:54 +0100

    update indentation of hammer-cli.groovy

--- workflows/test/hammer-cli.groovy
+++ workflows/test/hammer-cli.groovy
@@ -3,54 +3,53 @@ def ruby = version_map['ruby']
 
 pipeline {
 
-    agent { label 'sat6-build' }
+  agent { label 'sat6-build' }
 
-    options {
-      ansiColor('xterm')
-      disableConcurrentBuilds()
-      timestamps()
+  options {
+    ansiColor('xterm')
+    disableConcurrentBuilds()
+    timestamps()
+  }
+
+  stages {
+
+    stage('Setup Git Repos') {
+      steps {
+        deleteDir()
+        gitlab_clone_and_merge(plugin_name)
+      }
     }
 
-    stages {
-
-      stage('Setup Git Repos') {
-        steps {
-          deleteDir()
-          gitlab_clone_and_merge(plugin_name)
-        }
+    stage('Setup RVM') {
+      steps {
+        configureRVM(ruby)
       }
+    }
 
-      stage('Setup RVM') {
-        steps {
-          configureRVM(ruby)
-        }
-      }
+    stage('Run Tests') {
+      steps {
+        script {
+          if (plugin_name == 'hammer_cli_katello') {
+            test_command = 'rake'
+          } else {
+            test_command = 'rake ci:setup:minitest test'
+          }
 
-      stage('Run Tests') {
-        steps {
-          script {
-            if (plugin_name == 'hammer_cli_katello') {
-                test_command = 'rake'
-            } else {
-                test_command = 'rake ci:setup:minitest test'
+          try {
+
+            gitlabCommitStatus {
+              withRVM(['bundle install'], ruby)
+              withRVM(["bundle exec ${test_command} TESTOPTS='-v'"], ruby)
             }
 
-            try {
+          } finally {
 
-                gitlabCommitStatus {
-                    withRVM(['bundle install'], ruby)
-                    withRVM(["bundle exec ${test_command} TESTOPTS='-v'"], ruby)
-                }
+            archive "Gemfile.lock"
+            cleanupRVM(ruby)
 
-            } finally {
-
-                archive "Gemfile.lock"
-
-                cleanupRVM(ruby)
-
-            }
           }
         }
       }
     }
+  }
 }
