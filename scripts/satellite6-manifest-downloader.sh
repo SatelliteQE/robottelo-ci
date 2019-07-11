9f3edff5ccce41cccd5b27b6059c83f079d7372b
Jan Hutar <jhutar@redhat.com>
2019-07-11 09:17:36 +0200

    Enable manifest validation on download

--- scripts/satellite6-manifest-downloader.sh
+++ scripts/satellite6-manifest-downloader.sh
@@ -2,4 +2,5 @@ pip install -r requirements.txt
 source ${CONFIG_FILES}
 source config/fake_manifest.conf
 source config/subscription_config.conf
+export EXP_SUBS_FILE=config/robottelo-manifest-content.conf
 fab -D -H "root@${MANIFEST_SERVER_HOSTNAME}" relink_manifest
