0f7ff9cccd54053eb3b52b4e97b9525332e4e8b4
ntkathole <nikhilkathole2683@gmail.com>
2019-08-26 15:38:06 +0530

    Converted satellite6-upgrade-cleanup job to pipeline

--- scripts/satellite6-upgrade-cleanup.sh
+++
@@ -1,8 +0,0 @@
-# Setting Prerequisites
-pip install -r requirements.txt
-
-# Sourcing and exporting required env vars
-source ${CONFIG_FILES}
-source config/sat6_upgrade.conf
-
-fab -H $DOCKER_VM -u root docker_cleanup_containers
