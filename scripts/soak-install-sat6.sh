268293763dfb3f25138668f8ff7a55fca3dc1457
san7ket <san7ketjagtap@gmail.com>
2019-11-07 10:25:43 +0530

    Remove Soak pipeline, Since it was based on 6.3

--- scripts/soak-install-sat6.sh
+++
@@ -1,5 +0,0 @@
-ansible-playbook --private-key conf/id_rsa_soak -i conf/hosts.ini playbooks/satellite/installation.yaml
-
-ansible-playbook --private-key conf/id_rsa_soak -i conf/hosts.ini playbooks/soak-tests/sync-plan.yaml
-
-ansible-playbook --private-key conf/id_rsa_soak -i conf/hosts.ini playbooks/soak-tests/content-setup.yaml
