f1baa766c60ef870ae96736d9ef228d569487aca
Evgeni Golov <evgeni@redhat.com>
2019-11-20 08:22:24 +0100

    drop satellite 6.2 related jobs and defines

--- workflows/lib/branch_map.groovy
+++ workflows/lib/branch_map.groovy
@@ -1,12 +1,4 @@
 branch_map = [
-    'SATELLITE-6.2.0': [
-        'repo': 'Satellite 6.2 Source Files',
-        'version': '6.2.0',
-        'tool_belt_config': './configs/satellite/',
-        'foreman_branch': '1.11-stable',
-        'ruby': '2.0',
-        'packaging_job': null
-    ],
     'SATELLITE-6.3.0': [
         'repo': 'Satellite 6.3 Source Files',
         'version': '6.3.0',
