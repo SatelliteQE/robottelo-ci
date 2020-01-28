45a0234d0d4d769a19ee0ad66612093e97d7b050
Zach Huntington-Meath <zhunting@redhat.com>
2020-01-28 14:53:56 -0500

    Fix gemsnap deployment

--- workflows/deployGemsnap.groovy
+++ workflows/deployGemsnap.groovy
@@ -1,9 +1,7 @@
 node('sat6-build') {
     stage("Push to Open Platform") {
-        withCredentials([string(credentialsId: 'GEMSNAP_OPEN_PAAS_WEBHOOK_URL', variable: 'GEMSNAP_OPEN_PAAS_WEBHOOK_URL')]) {
-            sh "curl -k -XPOST ${GEMSNAP_63_PSI_WEBHOOK_URL}"
-            sh "curl -k -XPOST ${GEMSNAP_64_PSI_WEBHOOK_URL}"
-            sh "curl -k -XPOST ${GEMSNAP_63_PSI_WEBHOOK_URL}"
-	}
+    	sh "curl -k -XPOST ${GEMSNAP_64_PSI_WEBHOOK_URL}"
+    	sh "curl -k -XPOST ${GEMSNAP_65_PSI_WEBHOOK_URL}"
+    	sh "curl -k -XPOST ${GEMSNAP_66_PSI_WEBHOOK_URL}"
     }
 }
