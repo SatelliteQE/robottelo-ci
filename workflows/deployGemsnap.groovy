c4eab83f11b127956b46167bd77edfe50eade1e3
Zach Huntington-Meath <zhunting@redhat.com>
2020-01-28 14:18:43 -0500

    Update deploy gemsnap points to new gemsnap instances

--- workflows/deployGemsnap.groovy
+++ workflows/deployGemsnap.groovy
@@ -1,8 +1,9 @@
 node('sat6-build') {
     stage("Push to Open Platform") {
         withCredentials([string(credentialsId: 'GEMSNAP_OPEN_PAAS_WEBHOOK_URL', variable: 'GEMSNAP_OPEN_PAAS_WEBHOOK_URL')]) {
-            sh "curl -k -XPOST ${GEMSNAP_63_OPEN_PAAS_WEBHOOK_URL}"
-            sh "curl -k -XPOST ${GEMSNAP_64_OPEN_PAAS_WEBHOOK_URL}"
+            sh "curl -k -XPOST ${GEMSNAP_63_PSI_WEBHOOK_URL}"
+            sh "curl -k -XPOST ${GEMSNAP_64_PSI_WEBHOOK_URL}"
+            sh "curl -k -XPOST ${GEMSNAP_63_PSI_WEBHOOK_URL}"
 	}
     }
 }
