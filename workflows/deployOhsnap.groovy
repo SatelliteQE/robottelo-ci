4c396cfc47dbc2dd9a6a262b52225a128676350a
Zach Huntington-Meath <zhunting@redhat.com>
2020-01-28 14:15:55 -0500

    Update deploy ohsnap to direct to new Ohsnap instance

--- workflows/deployOhsnap.groovy
+++ workflows/deployOhsnap.groovy
@@ -1,7 +1,7 @@
 node('sat6-build') {
     stage("Push to Open Platform") {
-        withCredentials([string(credentialsId: 'OHSNAP_OPEN_PAAS_WEBHOOK_URL', variable: 'OHSNAP_OPEN_PAAS_WEBHOOK_URL')]) {
-            sh "curl -k -XPOST ${OHSNAP_OPEN_PAAS_WEBHOOK_URL}"
+        withCredentials([string(credentialsId: 'OHSNAP_PSI_WEBHOOK_URL', variable: 'OHSNAP_PSI_WEBHOOK_URL')]) {
+            sh "curl -k -XPOST ${OHSNAP_PSI_WEBHOOK_URL}"
 	}
     }
 }
