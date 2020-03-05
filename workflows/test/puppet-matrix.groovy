4e431bdacd4b4f4854c6eda95103db17345d034d
Evgeni Golov <evgeni@redhat.com>
2020-03-05 16:02:12 +0100

    add 6.7 puppet to matrix

--- workflows/test/puppet-matrix.groovy
+++ workflows/test/puppet-matrix.groovy
@@ -4,4 +4,5 @@ def puppet_ruby = [
     [puppet_version: '5.2.0', ruby_version: '2.4.3', satellite_versions: ['6.4.0']],
     [puppet_version: '5.2.0', ruby_version: '2.4.3', satellite_versions: ['6.5.0']],
     [puppet_version: '5.2.0', ruby_version: '2.4.3', satellite_versions: ['6.6.0']]
+    [puppet_version: '5.3.9', ruby_version: '2.4.3', satellite_versions: ['6.7.0']]
 ]
