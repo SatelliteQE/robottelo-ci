251fbb9e7b26d85efd02ce2038ce9587c8bf35f2
Lukas Pramuk <lpramuk@redhat.com>
2020-03-10 17:50:17 +0100

    Fix base image name inconsistency between automation-tools and robottelo
    (automation-tools requires rhel77-base vs. robottelo requires just rhel77)

--- scripts/satellite6-provisioning.sh
+++ scripts/satellite6-provisioning.sh
@@ -73,7 +73,7 @@ echo "DISCOVERY_ISO=${DISCOVERY_ISO}" >> build_env.properties
 # Run installation after writing the build_env.properties to make sure the
 # values are available for the post build actions, specially the foreman-debug
 # capturing.
-fab -D -H "root@${PROVISIONING_HOST}" "vm_create:target_image=${TARGET_IMAGE},source_image=${SOURCE_IMAGE},bridge=${BRIDGE}"
+fab -D -H "root@${PROVISIONING_HOST}" "vm_create:target_image=${TARGET_IMAGE},source_image=${SOURCE_IMAGE}-base,bridge=${BRIDGE}"
 fab -D -H "root@${SERVER_HOSTNAME}" "product_install:${DISTRIBUTION},sat_version=${SATELLITE_VERSION},test_in_stage=${STAGE_TEST},puppet4=${PUPPET4}"
 
 echo
