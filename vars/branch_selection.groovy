7387d68c21f627174381686f668ecb9958af7cfc
Lukas Pramuk <lpramuk@redhat.com>
2020-04-27 14:22:20 +0200

    Split 6.7 and 6.8 to 6.7.z and master branch

--- vars/branch_selection.groovy
+++ vars/branch_selection.groovy
@@ -10,6 +10,7 @@ def call(String satellite_version='6.8') {
         "6.4": "6.4.z",
         "6.5": "6.5.z",
         "6.6": "6.6.z",
+        "6.7": "6.7.z",
     ]
     def branch = satellite_version in branch_map ? branch_map[satellite_version] : "master"
     return branch
