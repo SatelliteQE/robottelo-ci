1d984fc6dce121c49a2b7366878e3e6579f1fec0
jyejare <jyejare@redhat.com>
2020-06-18 20:21:11 +0530

    Include Stubbed/Manual Tests in Polarion as waiting status

--- workflows/qe/satellite6-automation/satellite6-tiers.groovy
+++ workflows/qe/satellite6-automation/satellite6-tiers.groovy
@@ -276,7 +276,7 @@ stages {
           # Run sequential tests
           $(which py.test) -v --junit-xml="${ENDPOINT}-sequential-results.xml" \
             -o junit_suite_name="${ENDPOINT}-sequential" \
-            -m "${ENDPOINT} and run_in_one_thread and not stubbed ${EXTRA_MARKS}" \
+            -m "${ENDPOINT} and run_in_one_thread ${EXTRA_MARKS}" \
             ${TEST_TYPE}
           set -e
         '''
@@ -296,7 +296,7 @@ stages {
           # Run parallel tests
           $(which py.test) -v --junit-xml="${ENDPOINT}-parallel-results.xml" -n "${ROBOTTELO_WORKERS}" \
             -o junit_suite_name="${ENDPOINT}-parallel" \
-            -m "${ENDPOINT} and not run_in_one_thread and not stubbed ${EXTRA_MARKS}" \
+            -m "${ENDPOINT} and not run_in_one_thread ${EXTRA_MARKS}" \
             ${TEST_TYPE}
           set -e
         '''
