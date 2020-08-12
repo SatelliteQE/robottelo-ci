be333740d032fa05302a7f3367293b870145027b
Evgeni Golov <evgeni@redhat.com>
2020-08-12 15:16:39 +0200

    make the disttag configurable in the packaging jobs

--- workflows/packaging/pulpcorePackaging.groovy
+++ workflows/packaging/pulpcorePackaging.groovy
@@ -1,5 +1,6 @@
 package_version = env.gitlabTargetBranch.minus('PULPCORE-')
 packaging_repo = 'pulpcore-packaging'
 packaging_repo_project = 'satellite6'
+packaging_disttag = 'pc'
 tool_belt_repo_folder = "pulpcore_${package_version}"
 def tool_belt_config = './configs/pulpcore/'
