ba52196c6f49517828fbc45a83bab33f91248866
Patrick Creech <pcreech@redhat.com>
2020-10-13 14:50:13 -0400

    Remove el5, sles

--- workflows/6.9/releasePipelineAttributes.groovy
+++ workflows/6.9/releasePipelineAttributes.groovy
@@ -1,6 +1,6 @@
 def compose_rhel_versions = ['7']
-def tools_compose_rhel_versions = ['8', '7', '6', '5']
-def tools_compose_sles_versions = ['11.4', '12.3', '12.4', '15.1']
+def tools_compose_rhel_versions = ['8', '7', '6']
+def tools_compose_sles_versions = [ ]
 
 def os_versions = ['7']
 
@@ -30,13 +30,6 @@ def tools_repositories = [
     "Satellite Tools ${satellite_main_version} RHEL6 i386",
     "Satellite Tools ${satellite_main_version} RHEL6 ppc64",
     "Satellite Tools ${satellite_main_version} RHEL6 s390x",
-    "Satellite Tools ${satellite_main_version} RHEL5 x86_64",
-    "Satellite Tools ${satellite_main_version} RHEL5 i386",
-    "Satellite Tools ${satellite_main_version} RHEL5 s390x",
-    "Satellite Tools ${satellite_main_version} SLES11.4 x86_64",
-    "Satellite Tools ${satellite_main_version} SLES12.3 x86_64",
-    "Satellite Tools ${satellite_main_version} SLES12.4 x86_64",
-    "Satellite Tools ${satellite_main_version} SLES15.1 x86_64",
 ]
 
 def satellite_content_views = [
@@ -51,11 +44,6 @@ def tools_content_views = [
     "Tools ${satellite_main_version} RHEL8",
     "Tools ${satellite_main_version} RHEL7",
     "Tools ${satellite_main_version} RHEL6",
-    "Tools ${satellite_main_version} RHEL5",
-    "Tools ${satellite_main_version} SLES11.4",
-    "Tools ${satellite_main_version} SLES12.3",
-    "Tools ${satellite_main_version} SLES12.4",
-    "Tools ${satellite_main_version} SLES15.1",
 ]
 
 def content_views = satellite_content_views + capsule_content_views + tools_content_views
@@ -70,16 +58,10 @@ def tools_composite_content_views = [
     "Tools ${satellite_main_version} with RHEL8 Server",
     "Tools ${satellite_main_version} with RHEL7 Server",
     "Tools ${satellite_main_version} with RHEL6 Server",
-    "Tools ${satellite_main_version} with RHEL5 Server",
 ]
 def composite_content_views = satellite_composite_content_views + capsule_composite_content_views + tools_composite_content_views
 
-def tools_sles_content_views = [
-    "Tools ${satellite_main_version} SLES11.4",
-    "Tools ${satellite_main_version} SLES12.3",
-    "Tools ${satellite_main_version} SLES12.4",
-    "Tools ${satellite_main_version} SLES15.1",
-]
+def tools_sles_content_views = [ ]
 
 def satellite_activation_keys = [
     "satellite-${satellite_main_version}-qa-rhel7",
@@ -92,9 +74,5 @@ def tools_activation_keys = [
     "satellite-tools-${satellite_main_version}-qa-rhel7",
     "satellite-tools-${satellite_main_version}-qa-rhel6",
     "satellite-tools-${satellite_main_version}-qa-rhel5",
-    "satellite-tools-${satellite_main_version}-qa-sles11-4",
-    "satellite-tools-${satellite_main_version}-qa-sles12-3",
-    "satellite-tools-${satellite_main_version}-qa-sles12-4",
-    "satellite-tools-${satellite_main_version}-qa-sles15-1",
 ]
 def satellite_product = 'satellite'
