26a788f072a1b0eb9b63af7185b12cf23ee7c7a0
Patrick Creech <pcreech@localhost.localdomain>
2021-05-03 15:21:19 -0400

    Compensate for "SATELLITE-STREAM" branch name.

--- workflows/packaging/satellitePackaging.groovy
+++ workflows/packaging/satellitePackaging.groovy
@@ -1,4 +1,7 @@
 package_version = env.gitlabTargetBranch.minus('SATELLITE-')
+if (package_version == "STREAM" ) {
+    package_version = package_version.toLowerCase()  
+}
 packaging_repo = 'satellite-packaging'
 packaging_repo_project = 'satellite6'
 packaging_disttag = 'sat'
